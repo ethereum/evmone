63396bad89940c14d347d15c3c9878e7d9c4fc57
Alex Beregszaszi <alex@rtfs.hu>
2019-04-30 20:07:10 +0100

    Add header guard to public header

--- include/evmone/evmone.h
+++ include/evmone/evmone.h
@@ -2,8 +2,13 @@
 // Copyright 2018-2019 The evmone Authors.
 // Licensed under the Apache License, Version 2.0.
 
+#ifndef EVMONE_H
+#define EVMONE_H
+
 #include <evmc/evmc.h>
 
 extern "C" {
 evmc_instance* evmc_create_evmone() noexcept;
 }
+
+#endif  // EVMONE_H
