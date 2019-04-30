dc8ff14ae4a868850ddd52fdd1d5ff1eb9b5581d
Paweł Bylica <chfast@gmail.com>
2019-04-30 15:09:14 +0200

    Update copyright notice

--- include/evmone/evmone.h
+++ include/evmone/evmone.h
@@ -1,5 +1,5 @@
 // evmone: Fast Ethereum Virtual Machine implementation
-// Copyright 2018 Pawel Bylica.
+// Copyright 2018-2019 The evmone Authors.
 // Licensed under the Apache License, Version 2.0.
 
 #include <evmc/evmc.h>
