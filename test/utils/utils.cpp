c6f4f9a8bdfcd0c549afe2aa2f36ce2b1f61a510
Paweł Bylica <chfast@gmail.com>
2019-05-06 14:59:40 +0200

    test: Move from_hex() / to_hex() implementations to .cpp file

--- test/utils/utils.cpp
+++ test/utils/utils.cpp
@@ -10,6 +10,37 @@
 #include <iomanip>
 #include <iostream>
 
+bytes from_hex(std::string_view hex)
+{
+    bytes bs;
+    int b = 0;
+    for (size_t i = 0; i < hex.size(); ++i)
+    {
+        auto h = hex[i];
+        int v = (h <= '9') ? h - '0' : h - 'a' + 10;
+
+        if (i % 2 == 0)
+            b = v << 4;
+        else
+            bs.push_back(static_cast<uint8_t>(b | v));
+    }
+    return bs;
+}
+
+std::string to_hex(const uint8_t bytes[], size_t size)
+{
+    static const auto hex_chars = "0123456789abcdef";
+    std::string str;
+    str.reserve(size * 2);
+    for (size_t i = 0; i < size; ++i)
+    {
+        str.push_back(hex_chars[bytes[i] >> 4]);
+        str.push_back(hex_chars[bytes[i] & 0xf]);
+    }
+    return str;
+}
+
+
 void dump_analysis(const evmone::code_analysis& analysis)
 {
     using namespace evmone;
--- test/utils/utils.hpp
+++ test/utils/utils.hpp
@@ -9,32 +9,5 @@
 using bytes = std::basic_string<uint8_t>;
 using bytes_view = std::basic_string_view<uint8_t>;
 
-inline bytes from_hex(std::string_view hex) noexcept
-{
-    bytes bs;
-    int b = 0;
-    for (size_t i = 0; i < hex.size(); ++i)
-    {
-        auto h = hex[i];
-        int v = (h <= '9') ? h - '0' : h - 'a' + 10;
-
-        if (i % 2 == 0)
-            b = v << 4;
-        else
-            bs.push_back(static_cast<uint8_t>(b | v));
-    }
-    return bs;
-}
-
-inline std::string to_hex(const uint8_t bytes[], size_t size)
-{
-    static const auto hex_chars = "0123456789abcdef";
-    std::string str;
-    str.reserve(size * 2);
-    for (size_t i = 0; i < size; ++i)
-    {
-        str.push_back(hex_chars[bytes[i] >> 4]);
-        str.push_back(hex_chars[bytes[i] & 0xf]);
-    }
-    return str;
-}
+bytes from_hex(std::string_view hex);
+std::string to_hex(const uint8_t bytes[], size_t size);
