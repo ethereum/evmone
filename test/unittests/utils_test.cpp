b34379edaebaec98725832bbfbfbebd941e0fe0f
Paweł Bylica <chfast@gmail.com>
2019-05-20 14:24:35 +0200

    utils: Add hexx encoding format

--- test/unittests/utils_test.cpp
+++ test/unittests/utils_test.cpp
@@ -38,3 +38,21 @@ TEST(utils, hex_byte)
     b = 0xaa;
     EXPECT_EQ(hex(b), "aa");
 }
+
+TEST(utils, from_hexx)
+{
+    EXPECT_EQ(to_hex(from_hexx("")), "");
+
+    EXPECT_EQ(to_hex(from_hexx("(0xca)")), "");
+    EXPECT_EQ(to_hex(from_hexx("(1xca)")), "ca");
+    EXPECT_EQ(to_hex(from_hexx("(5xca)")), "cacacacaca");
+
+    EXPECT_EQ(to_hex(from_hexx("01(0x3a)02")), "0102");
+    EXPECT_EQ(to_hex(from_hexx("01(1x3a)02")), "013a02");
+    EXPECT_EQ(to_hex(from_hexx("01(2x3a)02")), "013a3a02");
+
+    EXPECT_EQ(to_hex(from_hexx("01(2x333)02(2x4444)03")), "01333333024444444403");
+    EXPECT_EQ(to_hex(from_hexx("01(4x333)02(4x4)03")), "0133333333333302444403");
+
+    EXPECT_EQ(to_hex(from_hexx("00")), "00");
+}
--- test/utils/utils.hpp
+++ test/utils/utils.hpp
@@ -25,3 +25,15 @@ inline std::string hex(evmc_opcode opcode) noexcept
 
 bytes from_hex(std::string_view hex);
 std::string to_hex(bytes_view bytes);
+
+/// Decodes the hexx encoded string.
+///
+/// The hexx encoding format is the hex format (base 16) with the extension
+/// for run-length encoding. The parser replaces expressions like
+///     `(` <num_repetitions> `x` <element> `)`
+/// with `<element>` repeated `<num_repetitions>` times.
+/// E.g. `(2x1d3)` is `1d31d3` in hex.
+///
+/// @param hexx  The hexx encoded string.
+/// @return      The decoded bytes.
+bytes from_hexx(const std::string& hexx);
