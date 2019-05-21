dea44c563a3eb4d5840004f097aada4f538816a9
Paweł Bylica <chfast@gmail.com>
2019-05-21 20:08:18 +0200

    bench: Load benchmarks from external files

--- test/bench/benchmarks.cpp
+++ test/bench/benchmarks.cpp
@@ -7,26 +7,30 @@
 
 #include <benchmark/benchmark.h>
 #include <test/utils/utils.hpp>
+#include <cctype>
 #include <fstream>
 #include <iomanip>
 #include <iostream>
+#include <memory>
 #include <sstream>
 
-using namespace benchmark;
 
-extern const bytes empty_code;
-extern const bytes sha1_divs_code;
-extern const bytes sha1_shifts_code;
-extern const bytes blake2b_shifts_code;
+#if __has_include(<filesystem>)
+#include <filesystem>
+namespace fs = std::filesystem;
+#else
+#include "filesystem.hpp"
+namespace fs = ghc::filesystem;
+#endif
+
+using namespace benchmark;
 
 namespace
 {
 constexpr auto gas_limit = std::numeric_limits<int64_t>::max();
 auto vm = evmc::vm{evmc_create_evmone()};
 
-bytes external_code;
-bytes external_input;
-std::string expected_output_hex;
+constexpr auto inputs_extension = ".inputs";
 
 inline evmc::result execute(bytes_view code, bytes_view input) noexcept
 {
@@ -52,123 +56,150 @@ void execute(State& state, bytes_view code, bytes_view input) noexcept
     state.counters["gas_rate"] = Counter(static_cast<double>(total_gas_used), Counter::kIsRate);
 }
 
-void empty(State& state) noexcept
+struct benchmark_case
 {
-    for (auto _ : state)
-        execute(empty_code, {});
-}
-BENCHMARK(empty);
+    std::shared_ptr<bytes> code;
+    bytes input;
+    bytes expected_output;
 
-void sha1_divs(State& state) noexcept
-{
-    const auto input_size = static_cast<size_t>(state.range(0));
-
-    auto abi_input =
-        from_hex("1605782b0000000000000000000000000000000000000000000000000000000000000020");
-
-    auto oss = std::ostringstream{};
-    oss << std::hex << std::setfill('0') << std::setw(64) << input_size;
-    abi_input += from_hex(oss.str());
-
-    abi_input.resize(abi_input.size() + input_size, 0);
-
-    execute(state, sha1_divs_code, abi_input);
-}
-BENCHMARK(sha1_divs)->Arg(0)->Arg(1351)->Arg(2737)->Arg(5311)->Arg(64 * 1024)->Unit(kMicrosecond);
-
-
-void sha1_shifts(State& state) noexcept
-{
-    const auto input_size = static_cast<size_t>(state.range(0));
-
-    auto abi_input =
-        from_hex("1605782b0000000000000000000000000000000000000000000000000000000000000020");
-
-    auto oss = std::ostringstream{};
-    oss << std::hex << std::setfill('0') << std::setw(64) << input_size;
-    abi_input += from_hex(oss.str());
-
-    abi_input.resize(abi_input.size() + input_size, 0);
-
-    execute(state, sha1_shifts_code, abi_input);
-}
-BENCHMARK(sha1_shifts)->Arg(0)->Arg(1351)->Arg(2737)->Arg(5311)->Arg(64 * 1024)->Unit(kMicrosecond);
-
-
-void blake2b_shifts(State& state) noexcept
-{
-    const auto input_size = static_cast<size_t>(state.range(0));
-
-    auto abi_input = from_hex(
-        "d299dac0"
-        "0000000000000000000000000000000000000000000000000000000000000060"
-        "0000000000000000000000000000000000000000000000000000000000000080"
-        "0000000000000000000000000000000000000000000000000000000000000040");
-
-    auto oss = std::ostringstream{};
-    oss << std::hex << std::setfill('0') << std::setw(64) << input_size;
-    abi_input += from_hex(oss.str());
-
-    abi_input.resize(abi_input.size() + input_size, 0);
-
-    execute(state, blake2b_shifts_code, abi_input);
-}
-BENCHMARK(blake2b_shifts)
-    ->Arg(0)
-    ->Arg(2805)
-    ->Arg(5610)
-    ->Arg(8415)
-    ->Arg(64 * 1024)
-    ->Unit(kMicrosecond);
-
-
-void external_evm_code(State& state) noexcept
-{
+    void operator()(State& state) noexcept
     {
-        auto r = execute(external_code, external_input);
-        const auto output_hex = to_hex({r.output_data, r.output_size});
-        if (output_hex != expected_output_hex)
         {
-            static auto error = "got: " + output_hex + "  expected: " + expected_output_hex;
-            state.SkipWithError(error.c_str());
-            return;
+            auto r = execute(*code, input);
+            if (r.status_code != EVMC_SUCCESS)
+            {
+                state.SkipWithError(("failure: " + std::to_string(r.status_code)).c_str());
+                return;
+            }
+
+            if (!expected_output.empty())
+            {
+                auto output = bytes_view{r.output_data, r.output_size};
+                if (output != expected_output)
+                {
+                    auto error =
+                        "got: " + to_hex(output) + "  expected: " + to_hex(expected_output);
+                    state.SkipWithError(error.c_str());
+                    return;
+                }
+            }
+        }
+
+        execute(state, *code, input);
+    }
+};
+
+
+void load_benchmark(fs::path path)
+{
+    auto base = benchmark_case{};
+    auto base_name = path.stem().string();
+
+    std::ifstream file{path};
+    std::string code_hex{std::istreambuf_iterator<char>{file}, std::istreambuf_iterator<char>{}};
+
+    code_hex.erase(
+        std::remove_if(code_hex.begin(), code_hex.end(), [](auto x) { return std::isspace(x); }),
+        code_hex.end());
+
+    base.code = std::make_shared<bytes>(from_hex(code_hex));
+
+    enum class state
+    {
+        name,
+        input,
+        expected_output
+    };
+
+    path.replace_extension(inputs_extension);
+    if (!fs::exists(path))
+    {
+        RegisterBenchmark(base_name.c_str(), base)->Unit(kMicrosecond);
+    }
+    else
+    {
+        auto st = state::name;
+        auto inputs_file = std::ifstream{path};
+        auto input = benchmark_case{};
+        auto name = std::string{};
+        for (std::string l; std::getline(inputs_file, l);)
+        {
+            switch (st)
+            {
+            case state::name:
+                if (l.empty())
+                    continue;
+                input = base;
+                name = base_name + '/' + std::move(l);
+                st = state::input;
+                break;
+
+            case state::input:
+                input.input = from_hexx(l);
+                st = state::expected_output;
+                break;
+
+            case state::expected_output:
+                input.expected_output = from_hexx(l);
+                RegisterBenchmark(name.c_str(), input)->Unit(kMicrosecond);
+                st = state::name;
+                break;
+            }
         }
     }
+}
 
-    execute(state, external_code, external_input);
+bool load_benchmarks_from_dir(const char* path)
+{
+    for (auto& e : fs::directory_iterator{path})
+    {
+        if (e.path().extension() == inputs_extension)
+            continue;
+
+        load_benchmark(e.path());
+    }
+    return true;
 }
 
 bool parseargs(int argc, char** argv)
 {
+    if (argc == 2)
+        return load_benchmarks_from_dir(argv[1]);
+
     if (argc != 4)
         return false;
 
     std::ifstream file{argv[1]};
     std::string code_hex{std::istreambuf_iterator<char>{file}, std::istreambuf_iterator<char>{}};
+    code_hex.erase(
+        std::remove_if(code_hex.begin(), code_hex.end(), [](auto x) { return std::isspace(x); }),
+        code_hex.end());
 
-    std::cout << "hex code length: " << code_hex.length() << std::endl;
-    external_code = from_hex(code_hex);
-
-    external_input = from_hex(argv[2]);
-    std::cout << "input size: " << external_input.size() << std::endl;
-
-    expected_output_hex = argv[3];
-    std::cout << "expected output: " << expected_output_hex << std::endl;
+    auto b = benchmark_case{};
+    b.code = std::make_shared<bytes>(from_hex(code_hex));
+    b.input = from_hex(argv[2]);
+    b.expected_output = from_hex(argv[3]);
+    RegisterBenchmark("external_evm_code", b)->Unit(kMicrosecond);
 
     return true;
 }
-
 }  // namespace
 
 int main(int argc, char** argv)
 {
-    Initialize(&argc, argv);
+    try
+    {
+        Initialize(&argc, argv);
 
-    if (parseargs(argc, argv))
-        RegisterBenchmark("external_evm_code", external_evm_code)->Unit(kMicrosecond);
-    else if (ReportUnrecognizedArguments(argc, argv))
-        return 1;
+        if (!parseargs(argc, argv) && ReportUnrecognizedArguments(argc, argv))
+            return 1;
 
-    RunSpecifiedBenchmarks();
-    return 0;
+        RunSpecifiedBenchmarks();
+        return 0;
+    }
+    catch (const std::exception& ex)
+    {
+        std::cerr << ex.what() << "\n";
+        return -1;
+    }
 }
