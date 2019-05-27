9ff182f10f7fa6a8da2f0a147902a75705c7ca76
Paweł Bylica <chfast@gmail.com>
2019-05-27 16:22:48 +0200

    test: Add helper macros for checking execution result

--- test/unittests/execution_fixture.hpp
+++ test/unittests/execution_fixture.hpp
@@ -2,10 +2,34 @@
 // Copyright 2019 The evmone Authors.
 // Licensed under the Apache License, Version 2.0.
 
+#pragma once
+
 #include <evmone/evmone.h>
 
 #include <gtest/gtest.h>
 #include <test/utils/host_mock.hpp>
+
+#define EXPECT_STATUS(STATUS_CODE)                                           \
+    EXPECT_EQ(result.status_code, STATUS_CODE);                              \
+    if constexpr (STATUS_CODE != EVMC_SUCCESS && STATUS_CODE != EVMC_REVERT) \
+    {                                                                        \
+        EXPECT_EQ(result.gas_left, 0);                                       \
+    }                                                                        \
+    (void)0
+
+#define EXPECT_GAS_USED(STATUS_CODE, GAS_USED)  \
+    EXPECT_EQ(result.status_code, STATUS_CODE); \
+    EXPECT_EQ(gas_used, GAS_USED)
+
+#define EXPECT_OUTPUT_INT(X)                                                                       \
+    {                                                                                              \
+        ASSERT_EQ(result.output_size, sizeof(intx::uint256));                                      \
+        evmc_bytes32 x;                                                                            \
+        intx::be::store(x.bytes, intx::uint256{X});                                                \
+        EXPECT_EQ(                                                                                 \
+            to_hex({result.output_data, result.output_size}), to_hex({x.bytes, sizeof(x.bytes)})); \
+    }                                                                                              \
+    (void)0
 
 class execution : public testing::Test, public MockedHost
 {
