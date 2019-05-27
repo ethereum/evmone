2a8f11fb098163a71866d913f70352aac10d21fc
Paweł Bylica <chfast@gmail.com>
2019-05-27 10:07:48 +0200

    Move main loop to a separate file

---
+++ lib/evmone/execution.cpp
@@ -0,0 +1,83 @@
+// evmone: Fast Ethereum Virtual Machine implementation
+// Copyright 2019 Pawel Bylica.
+// Licensed under the Apache License, Version 2.0.
+
+#include "execution.hpp"
+#include "analysis.hpp"
+
+#include <ethash/keccak.hpp>
+#include <evmc/helpers.hpp>
+#include <evmc/instructions.h>
+
+namespace evmone
+{
+extern const exec_fn_table op_table[];
+
+evmc_result execute(evmc_instance*, evmc_context* ctx, evmc_revision rev, const evmc_message* msg,
+    const uint8_t* code, size_t code_size) noexcept
+{
+    auto analysis = analyze(op_table[rev], rev, code, code_size);
+
+    execution_state state;
+    state.analysis = &analysis;
+    state.msg = msg;
+    state.code = code;
+    state.code_size = code_size;
+    state.host = evmc::HostContext{ctx};
+    state.gas_left = msg->gas;
+    state.rev = rev;
+    while (state.run)
+    {
+        auto& instr = analysis.instrs[state.pc];
+        if (instr.block_index >= 0)
+        {
+            auto& block = analysis.blocks[static_cast<size_t>(instr.block_index)];
+
+            state.gas_left -= block.gas_cost;
+            if (state.gas_left < 0)
+            {
+                state.status = EVMC_OUT_OF_GAS;
+                break;
+            }
+
+            if (static_cast<int>(state.stack.size()) < block.stack_req)
+            {
+                state.status = EVMC_STACK_UNDERFLOW;
+                break;
+            }
+
+            if (static_cast<int>(state.stack.size()) + block.stack_max > 1024)
+            {
+                state.status = EVMC_STACK_OVERFLOW;
+                break;
+            }
+
+            state.current_block_cost = block.gas_cost;
+        }
+
+        // Advance the PC not to allow jump opcodes to overwrite it.
+        ++state.pc;
+
+        instr.fn(state, instr.arg);
+    }
+
+    evmc_result result{};
+    result.status_code = state.status;
+    if (state.status == EVMC_SUCCESS || state.status == EVMC_REVERT)
+        result.gas_left = state.gas_left;
+
+    if (state.output_size > 0)
+    {
+        result.output_size = state.output_size;
+        auto output_data = static_cast<uint8_t*>(std::malloc(result.output_size));
+        std::memcpy(output_data, &state.memory[state.output_offset], result.output_size);
+        result.output_data = output_data;
+        result.release = [](const evmc_result* result) noexcept
+        {
+            std::free(const_cast<uint8_t*>(result->output_data));
+        };
+    }
+
+    return result;
+}
+}  // namespace evmone
--- lib/evmone/instructions.cpp
+++ lib/evmone/instructions.cpp
@@ -2,7 +2,6 @@
 // Copyright 2019 Pawel Bylica.
 // Licensed under the Apache License, Version 2.0.
 
-#include "execution.hpp"
 #include "analysis.hpp"
 
 #include <ethash/keccak.hpp>
@@ -1463,8 +1462,9 @@ constexpr exec_fn_table create_op_table_istanbul() noexcept
     auto table = create_op_table_constantinople();
     return table;
 }
+}  // namespace
 
-constexpr exec_fn_table op_table[] = {
+extern const exec_fn_table op_table[] = {
     create_op_table_frontier(),        // Frontier
     create_op_table_homestead(),       // Homestead
     create_op_table_homestead(),       // Tangerine Whistle
@@ -1474,75 +1474,4 @@ constexpr exec_fn_table op_table[] = {
     create_op_table_constantinople(),  // Petersburg
     create_op_table_istanbul(),        // Istanbul
 };
-}  // namespace
-
-
-evmc_result execute(evmc_instance*, evmc_context* ctx, evmc_revision rev, const evmc_message* msg,
-    const uint8_t* code, size_t code_size) noexcept
-{
-    auto analysis = analyze(op_table[rev], rev, code, code_size);
-
-    execution_state state;
-    state.analysis = &analysis;
-    state.msg = msg;
-    state.code = code;
-    state.code_size = code_size;
-    state.host = evmc::HostContext{ctx};
-    state.gas_left = msg->gas;
-    state.rev = rev;
-    while (state.run)
-    {
-        auto& instr = analysis.instrs[state.pc];
-        if (instr.block_index >= 0)
-        {
-            auto& block = analysis.blocks[static_cast<size_t>(instr.block_index)];
-
-            state.gas_left -= block.gas_cost;
-            if (state.gas_left < 0)
-            {
-                state.status = EVMC_OUT_OF_GAS;
-                break;
-            }
-
-            if (static_cast<int>(state.stack.size()) < block.stack_req)
-            {
-                state.status = EVMC_STACK_UNDERFLOW;
-                break;
-            }
-
-            if (static_cast<int>(state.stack.size()) + block.stack_max > 1024)
-            {
-                state.status = EVMC_STACK_OVERFLOW;
-                break;
-            }
-
-            state.current_block_cost = block.gas_cost;
-        }
-
-        // Advance the PC not to allow jump opcodes to overwrite it.
-        ++state.pc;
-
-        instr.fn(state, instr.arg);
-    }
-
-    evmc_result result{};
-    result.status_code = state.status;
-    if (state.status == EVMC_SUCCESS || state.status == EVMC_REVERT)
-        result.gas_left = state.gas_left;
-
-    if (state.output_size > 0)
-    {
-        result.output_size = state.output_size;
-        auto output_data = static_cast<uint8_t*>(std::malloc(result.output_size));
-        std::memcpy(output_data, &state.memory[state.output_offset], result.output_size);
-        result.output_data = output_data;
-        result.release = [](const evmc_result* result) noexcept
-        {
-            std::free(const_cast<uint8_t*>(result->output_data));
-        };
-    }
-
-    return result;
-}
-
 }  // namespace evmone
