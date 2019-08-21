7c7b5cda5553bd9840ee775f68510263e652312c
Paweł Bylica <chfast@gmail.com>
2019-08-21 21:25:05 +0200

    Use addmod()/mulmod() from intx

--- lib/evmone/instructions.cpp
+++ lib/evmone/instructions.cpp
@@ -130,7 +130,7 @@ void op_addmod(execution_state& state, instr_argument) noexcept
     const auto y = state.stack.pop();
     auto& m = state.stack.top();
 
-    m = m != 0 ? ((uint512{x} + uint512{y}) % uint512{m}).lo : 0;
+    m = m != 0 ? addmod(x, y, m) : 0;
 }
 
 void op_mulmod(execution_state& state, instr_argument) noexcept
@@ -139,7 +139,7 @@ void op_mulmod(execution_state& state, instr_argument) noexcept
     const auto y = state.stack.pop();
     auto& m = state.stack.top();
 
-    m = m != 0 ? ((uint512{x} * uint512{y}) % uint512{m}).lo : 0;
+    m = m != 0 ? mulmod(x, y, m) : 0;
 }
 
 void op_exp(execution_state& state, instr_argument) noexcept
