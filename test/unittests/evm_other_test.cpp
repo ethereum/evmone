639a1279e49cc205fd7b6ad91eb06eb1689b1cad
Paweł Bylica <chfast@gmail.com>
2019-08-28 15:41:02 +0200

    unittests: Move "block_stack_req_overflow" test to "evm_other"

--- test/unittests/evm_other_test.cpp
+++ test/unittests/evm_other_test.cpp
@@ -19,3 +19,14 @@ TEST_F(evm_other, evmone_loaded_program_relocation)
     execute(bytecode{} + OP_STOP + OP_ORIGIN);
     EXPECT_GAS_USED(EVMC_SUCCESS, 0);
 }
+
+TEST_F(evm_other, evmone_block_stack_req_overflow)
+{
+    // This tests constructs a code with single basic block which stack requirement is > int16 max.
+    // Such basic block can cause int16_t overflow during analysis.
+    // The CALL instruction is going to be used because it has -6 stack change.
+
+    const auto code = push(1) + 10 * OP_DUP1 + 5463 * OP_CALL;
+    execute(code);
+    EXPECT_STATUS(EVMC_STACK_UNDERFLOW);
+}
