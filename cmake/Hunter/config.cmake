f6718f0c364303e987c6c0649409b6fe60d16c73
Paweł Bylica <chfast@gmail.com>
2020-06-22 11:53:41 +0200

    build: Upgrade intx to 0.5.0

--- cmake/Hunter/config.cmake
+++ cmake/Hunter/config.cmake
@@ -4,7 +4,7 @@
 
 hunter_config(
     intx
-    VERSION 0.4.0
-    URL https://github.com/chfast/intx/archive/v0.4.0.tar.gz
-    SHA1 8a2a0b0efa64899db972166a9b3568a6984c61bc
+    VERSION 0.5.0
+    URL https://github.com/chfast/intx/archive/v0.5.0.tar.gz
+    SHA1 7f917cfe9046278d348a5cf36c20f9847d351807
 )
