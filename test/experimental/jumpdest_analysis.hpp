56ca120fe82ef009e823f75efc74b22c493dca59
Paweł Bylica <chfast@gmail.com>
2020-11-13 14:09:44 +0100

    test: Add experimental optimized jumpdest analysis

--- test/experimental/jumpdest_analysis.hpp
+++ test/experimental/jumpdest_analysis.hpp
@@ -2,6 +2,7 @@
 // Copyright 2020 The evmone Authors.
 // SPDX-License-Identifier: Apache-2.0
 
+#include <evmone/baseline.hpp>
 #include <cstdint>
 #include <vector>
 
@@ -13,4 +14,5 @@ inline bool is_jumpdest(const std::vector<bool>& jumpdest_map, size_t index) noe
 }
 
 std::vector<bool> build_jumpdest_map_vec1(const uint8_t* code, size_t code_size);
+bitset build_jumpdest_map_bitset1(const uint8_t* code, size_t code_size);
 }  // namespace evmone::experimental
