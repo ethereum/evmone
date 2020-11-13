1952dfc00b795262858a9773d55e26d9ffb73e08
Paweł Bylica <chfast@gmail.com>
2020-11-13 11:35:00 +0100

    Add evmone::experimental lib with jumpdest analysis
    Add new evmone::experimental library as a place for experimentation
    with different implementations of EVM components.
    Start with jumpdest analysis.

--- lib/evmone/instructions_opcodes.hpp
+++ lib/evmone/instructions_opcodes.hpp
@@ -1,8 +1,9 @@
 // evmone: Fast Ethereum Virtual Machine implementation
 // Copyright 2022 The evmone Authors.
 // SPDX-License-Identifier: Apache-2.0
-
 #pragma once
+
+#include <cstdint>
 
 namespace evmone
 {
