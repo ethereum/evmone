3b15985f21d99d51052e790e59b7fee6c0dd7424
Paweł Bylica <chfast@gmail.com>
2020-11-13 13:54:09 +0100

    test: Add experimental jumpdest analysis

---
+++ test/experimental/jumpdest_analysis.hpp
@@ -0,0 +1,16 @@
+// evmone: Fast Ethereum Virtual Machine implementation
+// Copyright 2020 The evmone Authors.
+// SPDX-License-Identifier: Apache-2.0
+
+#include <cstdint>
+#include <vector>
+
+namespace evmone::experimental
+{
+inline bool is_jumpdest(const std::vector<bool>& jumpdest_map, size_t index) noexcept
+{
+    return (index < jumpdest_map.size() && jumpdest_map[index]);
+}
+
+std::vector<bool> build_jumpdest_map_vec1(const uint8_t* code, size_t code_size);
+}  // namespace evmone::experimental
