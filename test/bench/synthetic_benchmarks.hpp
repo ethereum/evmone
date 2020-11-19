9efc716dec4f4dcea5fc0091eaa3166ee61c1bd0
Paweł Bylica <chfast@gmail.com>
2020-11-19 21:55:58 +0100

    bench: Add EVM instructions synthetic benchmarks
    This introduces a set of on-demand generated EVM bytecodes available in
    the evmone-bench tool. Each bytecode tries to stress a single
    "low-level" EVM instruction. Instructions are grouped by their stack
    requirements and there are also two main modes of the structure of the
    generated bytecodes.

---
+++ test/bench/synthetic_benchmarks.hpp
@@ -0,0 +1,9 @@
+// evmone: Fast Ethereum Virtual Machine implementation
+// Copyright 2020 The evmone Authors.
+// SPDX-License-Identifier: Apache-2.0
+#pragma once
+
+namespace evmone::test
+{
+void register_synthetic_benchmarks();
+}
