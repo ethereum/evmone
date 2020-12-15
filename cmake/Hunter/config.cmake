9a35501295aa04deb433ca43f37aacde7bb9f3b7
Paweł Bylica <chfast@gmail.com>
2020-12-15 11:13:49 +0100

    cmake: Upgrade dependencies
    - intx to 0.5.1
    - ethash to 0.6.0

--- cmake/Hunter/config.cmake
+++ cmake/Hunter/config.cmake
@@ -1,10 +1,17 @@
 # evmone: Fast Ethereum Virtual Machine implementation
-# Copyright 2018-2020 The evmone Authors.
+# Copyright 2018 The evmone Authors.
 # SPDX-License-Identifier: Apache-2.0
 
 hunter_config(
     intx
-    VERSION 0.5.0
-    URL https://github.com/chfast/intx/archive/v0.5.0.tar.gz
-    SHA1 7f917cfe9046278d348a5cf36c20f9847d351807
+    VERSION 0.5.1
+    URL https://github.com/chfast/intx/archive/v0.5.1.tar.gz
+    SHA1 743c46a82750143bd302a4394b7008a2112fc97b
+)
+
+hunter_config(
+    ethash
+    VERSION 0.6.0
+    URL https://github.com/chfast/ethash/archive/v0.6.0.tar.gz
+    SHA1 4bfa26b389d1f89a60053de04b2a29feab20f67b
 )
