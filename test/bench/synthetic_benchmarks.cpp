e2a053ea0e1dc588377c5999a3715f7df39c0092
Paweł Bylica <chfast@gmail.com>
2021-02-08 10:24:22 +0100

    bench: Dump synth benchmarks to disk

--- test/bench/synthetic_benchmarks.cpp
+++ test/bench/synthetic_benchmarks.cpp
@@ -8,6 +8,9 @@
 #include <evmc/instructions.h>
 #include <evmone/instruction_traits.hpp>
 
+#include <iostream>
+#include <fstream>
+
 using namespace benchmark;
 
 namespace evmone::test
@@ -272,6 +275,18 @@ void register_synthetic_benchmarks()
                 [&vm = vm, params](State& state) { execute(state, vm, generate_code(params)); })
                 ->Unit(kMicrosecond)
                 ->Iterations(1000);
+        }
+    }
+
+    if (std::getenv("BENCH_DUMP"))
+    {
+        for (const auto params : params_list)
+        {
+            auto filename = to_string(params);
+            std::replace(filename.begin(), filename.end(), '/', '_');
+            std::ofstream f{filename};
+            const auto code = generate_code(params);
+            std::copy(code.begin(), code.end(), std::ostreambuf_iterator(f));
         }
     }
 }
