1306ff17abc05d9c1452bade9eb98e54651024ae
Paweł Bylica <chfast@gmail.com>
2021-05-04 14:50:37 +0200

    Provide better execute() C++ API
    Change the execute API for Advanced interpreter to map the Baseline API.
    The new API requires to provide the execution state object (user can
    make better decision how to allocate it) and the code analysis (it is
    bound to the code itself so user can cache them together).

--- lib/evmone/execution.cpp
+++ lib/evmone/execution.cpp
@@ -8,22 +8,26 @@
 
 namespace evmone
 {
+evmc_result execute(AdvancedExecutionState& state, const AdvancedCodeAnalysis& analysis) noexcept
+{
+    state.analysis = &analysis;  // Allow accessing the analysis by instructions.
+
+    const auto* instr = &state.analysis->instrs[0];  // Start with the first instruction.
+    while (instr != nullptr)
+        instr = instr->fn(instr, state);
+
+    const auto gas_left =
+        (state.status == EVMC_SUCCESS || state.status == EVMC_REVERT) ? state.gas_left : 0;
+
+    return evmc::make_result(
+        state.status, gas_left, &state.memory[state.output_offset], state.output_size);
+}
+
 evmc_result execute(evmc_vm* /*unused*/, const evmc_host_interface* host, evmc_host_context* ctx,
     evmc_revision rev, const evmc_message* msg, const uint8_t* code, size_t code_size) noexcept
 {
     const auto analysis = analyze(rev, code, code_size);
-
-    auto state =
-        std::make_unique<AdvancedExecutionState>(*msg, rev, *host, ctx, code, code_size, analysis);
-
-    const auto* instr = &state->analysis->instrs[0];
-    while (instr != nullptr)
-        instr = instr->fn(instr, *state);
-
-    const auto gas_left =
-        (state->status == EVMC_SUCCESS || state->status == EVMC_REVERT) ? state->gas_left : 0;
-
-    return evmc::make_result(
-        state->status, gas_left, &state->memory[state->output_offset], state->output_size);
+    auto state = std::make_unique<AdvancedExecutionState>(*msg, rev, *host, ctx, code, code_size);
+    return execute(*state, analysis);
 }
 }  // namespace evmone
