787cd554af4e3c99a43c7ca3a9a37ec3fe8eb3af
Paweł Bylica <chfast@gmail.com>
2021-05-11 14:29:29 +0200

    Deduplicate add_tracer() code

--- lib/evmone/vm.hpp
+++ lib/evmone/vm.hpp
@@ -9,40 +9,42 @@
 
 namespace evmone
 {
-class VMTracer
+class VMTracer;
+
+class TracerListNode
 {
     std::unique_ptr<VMTracer> m_next_tracer;
 
 public:
-    virtual ~VMTracer() = default;
+    [[nodiscard]] VMTracer* get_next_tracer() const noexcept { return m_next_tracer.get(); }
 
-    void add_tracer(std::unique_ptr<VMTracer> tracer) noexcept
-    {
-        if (m_next_tracer)
-            m_next_tracer->add_tracer(std::move(tracer));
-        else
-            m_next_tracer = std::move(tracer);
-    }
+    inline void add_tracer(std::unique_ptr<VMTracer> tracer) noexcept;
+};
+
+class VMTracer : public TracerListNode
+{
+public:
+    virtual ~VMTracer() = default;
 
     void notify_execution_start() noexcept
     {
         onBeginExecution();
-        if (m_next_tracer)
-            m_next_tracer->notify_execution_start();
+        if (auto* tracer = get_next_tracer())
+            tracer->notify_execution_start();
     }
 
     void notify_execution_end() noexcept
     {
         onEndExecution();
-        if (m_next_tracer)
-            m_next_tracer->notify_execution_end();
+        if (auto* tracer = get_next_tracer())
+            tracer->notify_execution_end();
     }
 
     void notify_instruction_start(evmc_opcode opcode) noexcept
     {
         onOpcode(opcode);
-        if (m_next_tracer)
-            m_next_tracer->notify_instruction_start(opcode);
+        if (auto* tracer = get_next_tracer())
+            tracer->notify_instruction_start(opcode);
     }
 
 protected:
@@ -51,22 +53,25 @@ protected:
     virtual void onEndExecution() noexcept = 0;
 };
 
-/// The evmone EVMC instance.
-class VM : public evmc_vm
+void TracerListNode::add_tracer(std::unique_ptr<VMTracer> tracer) noexcept
 {
-    std::unique_ptr<VMTracer> m_tracer;
+    if (m_next_tracer)
+        m_next_tracer->add_tracer(std::move(tracer));
+    else
+        m_next_tracer = std::move(tracer);
+}
 
+/// The evmone EVMC instance.
+class VM : public evmc_vm, TracerListNode
+{
 public:
     inline constexpr VM() noexcept;
 
-    [[nodiscard]] VMTracer* get_tracer() const noexcept { return m_tracer.get(); }
+    using TracerListNode::add_tracer;
 
-    void add_tracer(std::unique_ptr<VMTracer> tracer) noexcept
+    [[nodiscard]] VMTracer* get_tracer() const noexcept
     {
-        if (m_tracer)
-            m_tracer->add_tracer(std::move(tracer));
-        else
-            m_tracer = std::move(tracer);
+        return TracerListNode::get_next_tracer();
     }
 };
 }  // namespace evmone
