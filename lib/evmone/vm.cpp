8c507be9f3dc027a75e8dce4ef6d8d123020b586
Paweł Bylica <chfast@gmail.com>
2021-05-13 21:13:24 +0200

    Refactor set_option() implementation

--- lib/evmone/vm.cpp
+++ lib/evmone/vm.cpp
@@ -25,16 +25,19 @@ constexpr evmc_capabilities_flagset get_capabilities(evmc_vm* /*vm*/) noexcept
     return EVMC_CAPABILITY_EVM1;
 }
 
-evmc_set_option_result set_option(evmc_vm* vm, char const* name, char const* value) noexcept
+evmc_set_option_result set_option(evmc_vm* vm, char const* c_name, char const* c_value) noexcept
 {
-    if (name[0] == 'O' && name[1] == '\0')
+    const auto name = (c_name != nullptr) ? std::string_view{c_name} : std::string_view{};
+    const auto value = (c_value != nullptr) ? std::string_view{c_value} : std::string_view{};
+
+    if (name == "O")
     {
-        if (value[0] == '0' && value[1] == '\0')  // O=0
+        if (value == "0")
         {
             vm->execute = evmone::baseline::execute;
             return EVMC_SET_OPTION_SUCCESS;
         }
-        else if (value[0] == '2' && value[1] == '\0')  // O=2
+        else if (value == "2")
         {
             vm->execute = evmone::execute;
             return EVMC_SET_OPTION_SUCCESS;
