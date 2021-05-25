5b5d0d0e6599082d9a19941064b6ed2a444b98e2
Alex Beregszaszi <alex@rtfs.hu>
2021-05-25 22:48:45 +0100

    cmake: Upgrade ethash to 0.7.0
    Build only ethash::keccak library.

--- cmake/Hunter/config.cmake
+++ cmake/Hunter/config.cmake
@@ -11,7 +11,8 @@ hunter_config(
 
 hunter_config(
     ethash
-    VERSION 0.6.0
-    URL https://github.com/chfast/ethash/archive/v0.6.0.tar.gz
-    SHA1 4bfa26b389d1f89a60053de04b2a29feab20f67b
+    VERSION 425f9cbb
+    URL https://github.com/chfast/ethash/archive/425f9cbb7a0a14719a94d6079b06dc1c16500061.tar.gz
+    SHA1 5c59a63d5af833a44dc5a5f5496087b35c423f8f
+    CMAKE_ARGS -DETHASH_BUILD_ETHASH=OFF -DETHASH_BUILD_TESTS=OFF
 )
