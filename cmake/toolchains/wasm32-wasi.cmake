66980b5becda1e8e716304eda6d792710fee8a8f
Paweł Bylica <chfast@gmail.com>
2021-05-26 13:57:01 +0200

    Fixup toolchain file

--- cmake/toolchains/wasm32-wasi.cmake
+++ cmake/toolchains/wasm32-wasi.cmake
@@ -1,8 +1,15 @@
+list(APPEND CMAKE_TRY_COMPILE_PLATFORM_VARIABLES WASI_SDK_PREFIX)
+
 if(NOT DEFINED WASI_SDK_PREFIX)
-    # Set default value of WASI_SDK_PREFIX
-    set(WASI_SDK_PREFIX /opt/wasi-sdk)
+    if(DEFINED ENV{WASI_SDK_PREFIX})
+        set(WASI_SDK_PREFIX $ENV{WASI_SDK_PREFIX})
+    else()
+        # Set default value of WASI_SDK_PREFIX
+        set(WASI_SDK_PREFIX /opt/wasi-sdk)
+    endif()
+else()
+    set(ENV{WASI_SDK_PREFIX} ${WASI_SDK_PREFIX})
 endif()
-set(CMAKE_TRY_COMPILE_PLATFORM_VARIABLES WASI_SDK_PREFIX)
 
 if(NOT WASI_SDK_PREFIX STREQUAL /opt/wasi-sdk)
     # Set --sysroot. Not necessary if WASI_SDK_PATH is /opt/wasi-sdk.
