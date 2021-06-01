e885d8a41736b724681648e8f9f7350c1c52ff54
Paweł Bylica <chfast@gmail.com>
2021-06-01 23:53:58 +0200

    baseline: Change stack requirements checks

--- lib/evmone/baseline.cpp
+++ lib/evmone/baseline.cpp
@@ -97,10 +97,13 @@ inline evmc_status_code check_requirements(
         return EVMC_OUT_OF_GAS;
 
     const auto stack_size = state.stack.size();
-    if (stack_size < metrics.stack_height_required)
+    if (INTX_UNLIKELY(stack_size == Stack::limit))
+    {
+        if (metrics.can_overflow_stack)
+            return EVMC_STACK_OVERFLOW;
+    }
+    else if (INTX_UNLIKELY(stack_size < metrics.stack_height_required))
         return EVMC_STACK_UNDERFLOW;
-    if (stack_size == Stack::limit && metrics.can_overflow_stack)
-        return EVMC_STACK_OVERFLOW;
 
     return EVMC_SUCCESS;
 }
