db3ebb9fd8f5a212053e7d699481678cf0d090df
Andrei Maiboroda <andrei@ethereum.org>
2021-08-12 14:02:24 +0200

    Unit test for advanced analyze of EOF1 code

--- lib/evmone/eof.hpp
+++ lib/evmone/eof.hpp
@@ -3,6 +3,7 @@
 // SPDX-License-Identifier: Apache-2.0
 #pragma once
 
+#include <evmc/utils.h>
 #include <stddef.h>
 #include <cstdint>
 
@@ -13,7 +14,7 @@ struct EOF1Header
     size_t code_size = 0;
     size_t data_size = 0;
 
-    size_t code_begin() const noexcept;
+    EVMC_EXPORT size_t code_begin() const noexcept;
 };
 
 // Checks if code starts with EOF FORMAT + MAGIC, doesn't validate the format.
@@ -21,5 +22,5 @@ bool is_eof_code(const uint8_t* code, size_t code_size) noexcept;
 
 // Reads the section sizes assuming that code has valid format.
 // (must be true for all EOF contracts on-chain)
-EOF1Header read_valid_eof1_header(const uint8_t* code) noexcept;
+EVMC_EXPORT EOF1Header read_valid_eof1_header(const uint8_t* code) noexcept;
 }  // namespace evmone
