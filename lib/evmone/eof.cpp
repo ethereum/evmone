62fcc7ff4d71cd1a4b9ae3c2fb2523a16c0df2f9
Andrei Maiboroda <andrei@ethereum.org>
2021-08-12 13:57:55 +0200

    EOF validation
    Co-authored-by: Paweł Bylica <chfast@gmail.com>

--- lib/evmone/eof.cpp
+++ lib/evmone/eof.cpp
@@ -13,6 +13,9 @@ namespace
 {
 constexpr uint8_t FORMAT = 0xef;
 constexpr uint8_t MAGIC[] = {0xca, 0xfe};
+constexpr uint8_t TERMINATOR = 0x00;
+constexpr uint8_t CODE_SECTION = 0x01;
+constexpr uint8_t DATA_SECTION = 0x02;
 }  // namespace
 
 size_t EOF1Header::code_begin() const noexcept
@@ -43,4 +46,107 @@ EOF1Header read_valid_eof1_header(const uint8_t* code) noexcept
     }
     return header;
 }
+
+uint8_t get_eof_version(const uint8_t* code, size_t code_size) noexcept
+{
+    return (code_size >= 4 && code[0] == FORMAT && code[1] == MAGIC[0] && code[2] == MAGIC[1]) ?
+               code[3] :
+               0;
+}
+
+std::pair<EOF1Header, EOFValidationErrror> validate_eof1(
+    const uint8_t* code, size_t code_size) noexcept
+{
+    enum class State
+    {
+        section_id,
+        section_size,
+        terminated
+    };
+
+    auto state = State::section_id;
+    uint8_t section_id = 0;
+    size_t section_sizes[3] = {0, 0, 0};
+    const auto* code_end = code + code_size;
+    auto it = code + sizeof(MAGIC) + 2;  // FORMAT + MAGIC + VERSION
+    while (it != code_end && state != State::terminated)
+    {
+        switch (state)
+        {
+        case State::section_id:
+        {
+            section_id = *it;
+            switch (section_id)
+            {
+            case TERMINATOR:
+                if (section_sizes[CODE_SECTION] == 0)
+                    return {{}, EOFValidationErrror::code_section_missing};
+                state = State::terminated;
+                break;
+            case DATA_SECTION:
+                if (section_sizes[CODE_SECTION] == 0)
+                    return {{}, EOFValidationErrror::code_section_missing};
+                [[fallthrough]];
+            case CODE_SECTION:
+                state = State::section_size;
+                break;
+            default:
+                return {{}, EOFValidationErrror::unknown_section_id};
+            }
+            break;
+        }
+        case State::section_size:
+        {
+            const auto size_hi = *it;
+            ++it;
+            if (it == code_end)
+                return {{}, EOFValidationErrror::incomplete_section_size};
+            const auto size_lo = *it;
+            const auto section_size = static_cast<size_t>(size_hi << 8) | size_lo;
+            if (section_size == 0)
+                return {{}, EOFValidationErrror::zero_section_size};
+
+            section_sizes[section_id] = section_size;
+            state = State::section_id;
+            break;
+        }
+        case State::terminated:
+            return {{}, EOFValidationErrror::impossible};
+        }
+
+        ++it;
+    }
+
+    if (state != State::terminated)
+        return {{}, EOFValidationErrror::section_headers_not_terminated};
+
+    const auto section_bodies_size = section_sizes[CODE_SECTION] + section_sizes[DATA_SECTION];
+    const auto remaining_code_size = static_cast<size_t>(code_end - it);
+    if (section_bodies_size != remaining_code_size)
+        return {{}, EOFValidationErrror::invalid_section_bodies_size};
+
+    return {{section_sizes[0], section_sizes[1]}, EOFValidationErrror::success};
+}
+
+EOFValidationErrror validate_eof(evmc_revision rev, const uint8_t* code, size_t code_size) noexcept
+{
+    if (!is_eof_code(code, code_size))
+        return EOFValidationErrror::invalid_prefix;
+
+    const auto version = get_eof_version(code, code_size);
+
+    switch (version)
+    {
+    default:
+        return EOFValidationErrror::eof_version_unknown;
+    case 1:
+    {
+        if (rev < EVMC_SHANGHAI)
+            return EOFValidationErrror::eof_version_unknown;
+        return validate_eof1(code, code_size).second;
+    }
+    }
+}
+
+
 }  // namespace evmone
--- lib/evmone/eof.hpp
+++ lib/evmone/eof.hpp
@@ -3,9 +3,11 @@
 // SPDX-License-Identifier: Apache-2.0
 #pragma once
 
+#include <evmc/evmc.h>
 #include <evmc/utils.h>
 #include <stddef.h>
 #include <cstdint>
+#include <utility>
 
 namespace evmone
 {
@@ -23,4 +25,34 @@ bool is_eof_code(const uint8_t* code, size_t code_size) noexcept;
 // Reads the section sizes assuming that code has valid format.
 // (must be true for all EOF contracts on-chain)
 EVMC_EXPORT EOF1Header read_valid_eof1_header(const uint8_t* code) noexcept;
+
+enum class EOFValidationErrror
+{
+    success,
+    starts_with_format,
+    invalid_prefix,
+    eof_version_mismatch,
+    eof_version_unknown,
+
+    incomplete_section_size,
+    code_section_missing,
+    unknown_section_id,
+    zero_section_size,
+    section_headers_not_terminated,
+    invalid_section_bodies_size,
+
+    initcode_failure,
+    impossible,
+};
+
+/// Determine the EOF version of the code by inspecting code's EOF prefix.
+/// If the prefix is missing or invalid, the 0 is returned meaning legacy code.
+uint8_t get_eof_version(const uint8_t* code, size_t code_size) noexcept;
+
+std::pair<EOF1Header, EOFValidationErrror> validate_eof1(
+    const uint8_t* code, size_t code_size) noexcept;
+
+EVMC_EXPORT EOFValidationErrror validate_eof(
+    evmc_revision rev, const uint8_t* code, size_t code_size) noexcept;
+
 }  // namespace evmone
