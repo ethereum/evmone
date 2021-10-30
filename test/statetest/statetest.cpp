3852ebb7b2b3bf97c8dde4b885a02edc29fc3e2c
Paweł Bylica <chfast@gmail.com>
2021-10-30 20:29:41 +0200

    Compiler warnings WIP

--- test/statetest/statetest.cpp
+++ test/statetest/statetest.cpp
@@ -84,7 +84,7 @@ int main(int argc, char* argv[])
         bool trace_flag = false;
         app.add_flag("--trace", trace_flag, "Enable EVM tracing");
 
-        CLI11_PARSE(app, argc, argv);
+        CLI11_PARSE(app, argc, argv)
 
         evmc::VM vm{evmc_create_evmone(), {{"O", "0"}}};
 
