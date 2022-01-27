5fdf8d06a7a9d7f52790cf6197a626ea336c6697
rodiazet <radek.zagorowicz@gmail.com>
2022-01-27 17:08:30 +0100

    Enable and fix of clear stack test

--- test/unittests/execution_state_test.cpp
+++ test/unittests/execution_state_test.cpp
@@ -138,26 +138,26 @@ TEST(execution_state, reset_advanced)
     }
 }
 
-// TEST(execution_state, stack_clear)
-// {
-//     evmone::Stack stack;
-//
-//     stack.clear();
-//     EXPECT_EQ(stack.size(), 0);
-//     EXPECT_EQ(stack.top_item + 1, stack.storage);
-//
-//     stack.push({});
-//     EXPECT_EQ(stack.size(), 1);
-//     EXPECT_EQ(stack.top_item, stack.storage);
-//
-//     stack.clear();
-//     EXPECT_EQ(stack.size(), 0);
-//     EXPECT_EQ(stack.top_item + 1, stack.storage);
-//
-//     stack.clear();
-//     EXPECT_EQ(stack.size(), 0);
-//     EXPECT_EQ(stack.top_item + 1, stack.storage);
-// }
+TEST(execution_state, stack_clear)
+{
+    evmone::Stack stack;
+
+    stack.clear();
+    EXPECT_EQ(stack.size(), 0);
+    EXPECT_EQ(stack.top_item + 1, stack.storage());
+
+    stack.push({});
+    EXPECT_EQ(stack.size(), 1);
+    EXPECT_EQ(stack.top_item, stack.storage());
+
+    stack.clear();
+    EXPECT_EQ(stack.size(), 0);
+    EXPECT_EQ(stack.top_item + 1, stack.storage());
+
+    stack.clear();
+    EXPECT_EQ(stack.size(), 0);
+    EXPECT_EQ(stack.top_item + 1, stack.storage());
+}
 
 TEST(execution_state, const_stack)
 {
