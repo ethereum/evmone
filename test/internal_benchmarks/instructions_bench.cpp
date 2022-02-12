6df34b6ef90b3e2c70e9ca5615d78c6cd6802b37
Paweł Bylica <chfast@gmail.com>
2022-02-12 23:58:28 +0100

    loop_v5

--- test/internal_benchmarks/instructions_bench.cpp
+++ test/internal_benchmarks/instructions_bench.cpp
@@ -17,11 +17,14 @@ using instr_v3 = evmc_status_code (*)(
     size_t pc, uint256* top, const uint256* bottom, ExecutionState& state) noexcept;
 using instr_v4 = evmc_status_code (*)(
     size_t pc, uint256* top, const uint256* bottom, int64_t gas, ExecutionState& state) noexcept;
+using instr_v5 = evmc_status_code (*)(
+    size_t pc, uint256* bottom, int size, ExecutionState& state) noexcept;
 
 instr_v1 instr_table_v1[] = {nullptr};
 instr_v2 instr_table_v2[] = {nullptr};
 instr_v3 instr_table_v3[] = {nullptr};
 instr_v4 instr_table_v4[] = {nullptr};
+instr_v5 instr_table_v5[] = {nullptr};
 
 [[gnu::noinline]] evmc_status_code loop_v1(const uint8_t* code, ExecutionState& state) noexcept
 {
@@ -44,6 +47,12 @@ instr_v4 instr_table_v4[] = {nullptr};
     int64_t gas, ExecutionState& state) noexcept
 {
     return instr_table_v4[code[0]](0, top, bottom, gas, state);
+}
+
+[[gnu::noinline]] evmc_status_code loop_v5(
+    const uint8_t* code, uint256* bottom, int size, ExecutionState& state) noexcept
+{
+    return instr_table_v5[code[0]](0, bottom, size, state);
 }
 
 evmc_status_code add_v1(size_t pc, ExecutionState& state) noexcept
@@ -127,6 +136,21 @@ evmc_status_code add_v4a(
 
     pc += 1;
     [[clang::musttail]] return instr_table_v4[state.code[pc]](pc, stack - 1, bottom, gas, state);
+}
+
+evmc_status_code add_v5(size_t pc, uint256* bottom, int size, ExecutionState& state) noexcept
+{
+    if (INTX_UNLIKELY((size + 1) < 2))
+        return EVMC_STACK_UNDERFLOW;
+
+    state.gas_left -= 3;
+    if (INTX_UNLIKELY(state.gas_left < 0))
+        return EVMC_OUT_OF_GAS;
+
+    bottom[size - 1] += bottom[size];
+
+    pc += 1;
+    [[clang::musttail]] return instr_table_v5[state.code[pc]](pc, bottom, size - 1, state);
 }
 
 
@@ -227,3 +251,27 @@ static void run_v4(benchmark::State& state)
 }
 BENCHMARK_TEMPLATE(run_v4, add_v4);
 BENCHMARK_TEMPLATE(run_v4, add_v4a);
+
+
+template <instr_v5 Instr>
+static void run_v5(benchmark::State& state)
+{
+    instr_table_v5[0] = Instr;
+    uint8_t code[1024]{};
+    evmone::ExecutionState es;
+    es.code = {code, std::size(code)};
+    es.gas_left = std::numeric_limits<int64_t>::max();
+    for (uint64_t i = 0; i < 1024; ++i)
+        es.stack.push(i);
+
+    es.stack.clear();
+    const auto bottom = es.stack.top_item;
+    for (auto _ : state)
+    {
+        es.stack.top_item = bottom;
+        const auto r = loop_v5(code, bottom, 1023, es);
+        if (INTX_UNLIKELY(r != EVMC_STACK_UNDERFLOW))
+            state.SkipWithError("wrong exit code");
+    }
+}
+BENCHMARK_TEMPLATE(run_v5, add_v5);
