56a20bebd811153c8fb3931a95b25b6bfce86b8b
Andrei Maiboroda <andrei@ethereum.org>
2022-03-09 16:00:51 +0100

    Update EVMC to get new instructions

--- lib/evmone/instructions_traits.hpp
+++ lib/evmone/instructions_traits.hpp
@@ -164,6 +164,8 @@ constexpr inline GasCostTable gas_costs = []() noexcept {
     table[EVMC_SHANGHAI][OP_PUSH0] = 2;
 
     table[EVMC_CANCUN] = table[EVMC_SHANGHAI];
+    table[EVMC_CANCUN][OP_RJUMP] = 5;
+    table[EVMC_CANCUN][OP_RJUMPI] = 7;
 
     return table;
 }();
@@ -283,6 +285,8 @@ constexpr inline std::array<Traits, 256> traits = []() noexcept {
     table[OP_MSIZE] = {"MSIZE", 0, 1, EVMC_FRONTIER};
     table[OP_GAS] = {"GAS", 0, 1, EVMC_FRONTIER};
     table[OP_JUMPDEST] = {"JUMPDEST", 0, 0, EVMC_FRONTIER};
+    table[OP_RJUMP] = {"RJUMP", 0, 0, EVMC_CANCUN};
+    table[OP_RJUMPI] = {"RJUMPI", 1, -1, EVMC_CANCUN};
 
     table[OP_PUSH0] = {"PUSH0", 0, 1, EVMC_SHANGHAI};
 
