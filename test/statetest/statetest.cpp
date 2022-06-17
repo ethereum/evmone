d77fac40084dbf650cab992d6029567719c31187
Paweł Bylica <pawel@hepcolgum.band>
2022-06-17 14:03:58 +0200

    statetest: Register test files with GTest

--- test/statetest/statetest.cpp
+++ test/statetest/statetest.cpp
@@ -3,10 +3,51 @@
 // SPDX-License-Identifier: Apache-2.0
 
 #include "statetest.hpp"
+#include <gtest/gtest.h>
+#include <filesystem>
+#include <iostream>
+
+namespace fs = std::filesystem;
+
+namespace
+{
+class StateTest : public testing::Test
+{
+    fs::path m_json_test_file;
+
+public:
+    explicit StateTest(fs::path json_test_file) noexcept
+      : m_json_test_file{std::move(json_test_file)}
+    {}
+
+    void TestBody() final { std::cout << m_json_test_file << "\n"; }
+};
+}  // namespace
 
 int main(int argc, char* argv[])
 {
-    (void)argc;
-    (void)argv;
-    return 0;
+    testing::InitGoogleTest(&argc, argv);  // Process GoogleTest flags.
+
+    if (argc != 2)
+    {
+        std::cerr << "Missing argument with the path to the tests directory\n";
+        return -1;
+    }
+
+    std::vector<fs::path> test_files;
+    const fs::path root_test_dir{argv[1]};
+    std::copy_if(fs::recursive_directory_iterator{root_test_dir},
+        fs::recursive_directory_iterator{}, std::back_inserter(test_files),
+        [](const fs::directory_entry& entry) {
+            return entry.is_regular_file() && entry.path().extension() == ".json";
+        });
+    std::sort(test_files.begin(), test_files.end());
+    for (const auto& p : test_files)
+    {
+        const auto d = fs::relative(p, root_test_dir);
+        testing::RegisterTest(d.parent_path().string().c_str(), d.stem().string().c_str(), nullptr,
+            nullptr, p.string().c_str(), 0, [p]() -> testing::Test* { return new StateTest(p); });
+    }
+
+    return RUN_ALL_TESTS();
 }
