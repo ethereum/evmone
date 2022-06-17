9992069386bde23fab81387d5e03bce1fd24b0ce
Paweł Bylica <pawel@hepcolgum.band>
2022-06-17 12:33:21 +0200

    statetest: Add evmone-statetest stub
    The evmone-statetest will be the tool to execute official Ethereum
    State Tests.

---
+++ test/statetest/statetest.cpp
@@ -0,0 +1,12 @@
+// evmone: Fast Ethereum Virtual Machine implementation
+// Copyright 2022 The evmone Authors.
+// SPDX-License-Identifier: Apache-2.0
+
+#include "statetest.hpp"
+
+int main(int argc, char* argv[])
+{
+    (void)argc;
+    (void)argv;
+    return 0;
+}
---
+++ test/statetest/statetest.hpp
@@ -0,0 +1,7 @@
+// evmone: Fast Ethereum Virtual Machine implementation
+// Copyright 2022 The evmone Authors.
+// SPDX-License-Identifier: Apache-2.0
+#pragma once
+
+namespace evmone::test
+{}
