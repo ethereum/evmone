5b8553cbe487c37d74998b425bb12aec63ae6d96
Paweł Bylica <pawel@hepcolgum.band>
2022-06-17 11:00:56 +0200

    state: Add basic struct for Account representation

---
+++ test/state/account.hpp
@@ -0,0 +1,38 @@
+// evmone: Fast Ethereum Virtual Machine implementation
+// Copyright 2021 The evmone Authors.
+// SPDX-License-Identifier: Apache-2.0
+#pragma once
+
+#include <evmc/evmc.hpp>
+#include <intx/intx.hpp>
+#include <unordered_map>
+
+namespace evmone::state
+{
+using evmc::address;
+using evmc::bytes;
+using evmc::bytes32;
+
+/// The representation of the account storage value.
+struct StorageValue
+{
+    /// The current value.
+    bytes32 current{};
+};
+
+/// The state account.
+struct Account
+{
+    /// The account nonce.
+    uint64_t nonce = 0;
+
+    /// The account balance.
+    intx::uint256 balance;
+
+    /// The account storage map.
+    std::unordered_map<bytes32, StorageValue> storage;
+
+    /// The account code.
+    bytes code;
+};
+}  // namespace evmone::state
