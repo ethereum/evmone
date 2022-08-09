480023e9ffa8b78e7fee2db8695d3b0ca1e4fcb1
Paweł Bylica <pawel@hepcolgum.band>
2022-08-09 19:10:00 +0200

    Implement gas refund for SSTORE

--- lib/evmone/instructions_storage.cpp
+++ lib/evmone/instructions_storage.cpp
@@ -6,6 +6,91 @@
 
 namespace evmone::instr::core
 {
+namespace
+{
+/// The gas cost specification for storage instructions.
+struct StorageCostSpec
+{
+    bool net_cost;        ///< Is this net gas cost metering schedule?
+    int16_t warm_access;  ///< Storage warm access cost, YP: G_{warmaccess}
+    int16_t set;          ///< Storage addition cost, YP: G_{sset}
+    int16_t reset;        ///< Storage modification cost, YP: G_{sreset}
+    int16_t clear;        ///< Storage deletion refund, YP: R_{sclear}
+};
+
+/// Table of gas cost specification for storage instructions per EVM revision.
+/// TODO: This can be moved to instruction traits and be used in other places: e.g.
+///       SLOAD cost, replacement for warm_storage_read_cost.
+constexpr auto storage_cost_spec = []() noexcept {
+    std::array<StorageCostSpec, EVMC_MAX_REVISION + 1> tbl{};
+
+    // Legacy cost schedule.
+    for (auto rev : {EVMC_FRONTIER, EVMC_HOMESTEAD, EVMC_TANGERINE_WHISTLE, EVMC_SPURIOUS_DRAGON,
+             EVMC_BYZANTIUM, EVMC_PETERSBURG})
+        tbl[rev] = {false, 200, 20000, 5000, 15000};
+
+    // Net cost schedule.
+    tbl[EVMC_CONSTANTINOPLE] = {true, 200, 20000, 5000, 15000};
+    tbl[EVMC_ISTANBUL] = {true, 800, 20000, 5000, 15000};
+    tbl[EVMC_BERLIN] = {
+        true, instr::warm_storage_read_cost, 20000, 5000 - instr::cold_sload_cost, 15000};
+    tbl[EVMC_LONDON] = {
+        true, instr::warm_storage_read_cost, 20000, 5000 - instr::cold_sload_cost, 4800};
+    tbl[EVMC_PARIS] = tbl[EVMC_LONDON];
+    tbl[EVMC_SHANGHAI] = tbl[EVMC_LONDON];
+    tbl[EVMC_CANCUN] = tbl[EVMC_LONDON];
+    return tbl;
+}();
+
+
+struct StorageStoreCost
+{
+    int16_t gas_cost;
+    int16_t gas_refund;
+};
+
+// The lookup table of SSTORE costs by the storage update status.
+constexpr auto sstore_costs = []() noexcept {
+    std::array<std::array<StorageStoreCost, EVMC_STORAGE_MODIFIED_RESTORED + 1>,
+        EVMC_MAX_REVISION + 1>
+        tbl{};
+
+    for (size_t rev = EVMC_FRONTIER; rev <= EVMC_MAX_REVISION; ++rev)
+    {
+        auto& e = tbl[rev];
+        if (const auto c = storage_cost_spec[rev]; !c.net_cost)  // legacy
+        {
+            e[EVMC_STORAGE_ADDED] = {c.set, 0};
+            e[EVMC_STORAGE_DELETED] = {c.reset, c.clear};
+            e[EVMC_STORAGE_MODIFIED] = {c.reset, 0};
+            e[EVMC_STORAGE_ASSIGNED] = e[EVMC_STORAGE_MODIFIED];
+            e[EVMC_STORAGE_DELETED_ADDED] = e[EVMC_STORAGE_ADDED];
+            e[EVMC_STORAGE_MODIFIED_DELETED] = e[EVMC_STORAGE_DELETED];
+            e[EVMC_STORAGE_DELETED_RESTORED] = e[EVMC_STORAGE_ADDED];
+            e[EVMC_STORAGE_ADDED_DELETED] = e[EVMC_STORAGE_DELETED];
+            e[EVMC_STORAGE_MODIFIED_RESTORED] = e[EVMC_STORAGE_MODIFIED];
+        }
+        else  // net cost
+        {
+            e[EVMC_STORAGE_ASSIGNED] = {c.warm_access, 0};
+            e[EVMC_STORAGE_ADDED] = {c.set, 0};
+            e[EVMC_STORAGE_DELETED] = {c.reset, c.clear};
+            e[EVMC_STORAGE_MODIFIED] = {c.reset, 0};
+            e[EVMC_STORAGE_DELETED_ADDED] = {c.warm_access, static_cast<int16_t>(-c.clear)};
+            e[EVMC_STORAGE_MODIFIED_DELETED] = {c.warm_access, c.clear};
+            e[EVMC_STORAGE_DELETED_RESTORED] = {
+                c.warm_access, static_cast<int16_t>(c.reset - c.warm_access - c.clear)};
+            e[EVMC_STORAGE_ADDED_DELETED] = {
+                c.warm_access, static_cast<int16_t>(c.set - c.warm_access)};
+            e[EVMC_STORAGE_MODIFIED_RESTORED] = {
+                c.warm_access, static_cast<int16_t>(c.reset - c.warm_access)};
+        }
+    }
+
+    return tbl;
+}();
+}  // namespace
+
 evmc_status_code sload(StackTop stack, ExecutionState& state) noexcept
 {
     auto& x = stack.top();
@@ -38,63 +123,18 @@ evmc_status_code sstore(StackTop stack, ExecutionState& state) noexcept
     const auto key = intx::be::store<evmc::bytes32>(stack.pop());
     const auto value = intx::be::store<evmc::bytes32>(stack.pop());
 
-    int cost = 0;
-    if (state.rev >= EVMC_BERLIN &&
-        state.host.access_storage(state.msg->recipient, key) == EVMC_ACCESS_COLD)
-        cost = instr::cold_sload_cost;
-
+    const auto gas_cost_cold =
+        (state.rev >= EVMC_BERLIN &&
+            state.host.access_storage(state.msg->recipient, key) == EVMC_ACCESS_COLD) ?
+            instr::cold_sload_cost :
+            0;
     const auto status = state.host.set_storage(state.msg->recipient, key, value);
 
-    if (state.rev <= EVMC_BYZANTIUM || state.rev == EVMC_PETERSBURG)  // legacy
-    {
-        switch (status)
-        {
-        case EVMC_STORAGE_ASSIGNED:
-        case EVMC_STORAGE_MODIFIED_DELETED:
-        case EVMC_STORAGE_ADDED_DELETED:
-        case EVMC_STORAGE_MODIFIED_RESTORED:
-        case EVMC_STORAGE_MODIFIED:
-        case EVMC_STORAGE_DELETED:
-            cost = 5000;
-            break;
-        case EVMC_STORAGE_ADDED:
-        case EVMC_STORAGE_DELETED_ADDED:
-        case EVMC_STORAGE_DELETED_RESTORED:
-            cost = 20000;
-            break;
-        }
-    }
-    else  // net gas cost metering
-    {
-        switch (status)
-        {
-        case EVMC_STORAGE_ASSIGNED:
-        case EVMC_STORAGE_DELETED_ADDED:
-        case EVMC_STORAGE_DELETED_RESTORED:
-        case EVMC_STORAGE_MODIFIED_DELETED:
-        case EVMC_STORAGE_ADDED_DELETED:
-        case EVMC_STORAGE_MODIFIED_RESTORED:
-            if (state.rev >= EVMC_BERLIN)
-                cost += instr::warm_storage_read_cost;
-            else if (state.rev == EVMC_ISTANBUL)
-                cost = 800;
-            else
-                cost = 200;  // Constantinople
-            break;
-        case EVMC_STORAGE_MODIFIED:
-        case EVMC_STORAGE_DELETED:
-            if (state.rev >= EVMC_BERLIN)
-                cost += 5000 - instr::cold_sload_cost;
-            else
-                cost = 5000;
-            break;
-        case EVMC_STORAGE_ADDED:
-            cost += 20000;
-            break;
-        }
-    }
-    if ((state.gas_left -= cost) < 0)
+    const auto [gas_cost_warm, gas_refund] = sstore_costs[state.rev][status];
+    const auto gas_cost = gas_cost_warm + gas_cost_cold;
+    if ((state.gas_left -= gas_cost) < 0)
         return EVMC_OUT_OF_GAS;
+    state.gas_refund += gas_refund;
     return EVMC_SUCCESS;
 }
 }  // namespace evmone::instr::core
