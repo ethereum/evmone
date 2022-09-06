447864e7426ee116ffa3b19912a16c7d4dcdca3a
Andrei Maiboroda <andrei@ethereum.org>
2022-09-06 15:09:57 +0200

    Reset gas refund counter in ExecutionState

--- test/unittests/execution_state_test.cpp
+++ test/unittests/execution_state_test.cpp
@@ -84,6 +84,7 @@ TEST(execution_state, reset_advanced)
 
     evmone::advanced::AdvancedExecutionState st;
     st.gas_left = 1;
+    st.gas_refund = 2;
     st.stack.push({});
     st.memory.grow(64);
     st.msg = &msg;
@@ -97,6 +98,7 @@ TEST(execution_state, reset_advanced)
     st.analysis.advanced = &analysis;
 
     EXPECT_EQ(st.gas_left, 1);
+    EXPECT_EQ(st.gas_refund, 2);
     EXPECT_EQ(st.stack.size(), 1);
     EXPECT_EQ(st.memory.size(), 64);
     EXPECT_EQ(st.msg, &msg);
@@ -121,6 +123,7 @@ TEST(execution_state, reset_advanced)
         // TODO: We are not able to test HostContext with current API. It may require an execution
         //       test.
         EXPECT_EQ(st.gas_left, 13);
+        EXPECT_EQ(st.gas_refund, 0);
         EXPECT_EQ(st.stack.size(), 0);
         EXPECT_EQ(st.memory.size(), 0);
         EXPECT_EQ(st.msg, &msg2);
