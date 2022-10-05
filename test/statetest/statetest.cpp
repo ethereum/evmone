db31a4fcf4af35602b79b01860d08560f5db0a71
Jakub Szymański <54314608+JSzymanskiJS@users.noreply.github.com>
2022-10-05 10:58:04 +0200

    statetest: Load tests from multiple dirs/files
    Allow passing multiple paths to evmone-statetest to load tests from.
    
    Co-authored-by: Paweł Bylica <pawel@hepcolgum.band>

--- test/statetest/statetest.cpp
+++ test/statetest/statetest.cpp
@@ -20,33 +20,38 @@ public:
     void TestBody() final { evmone::test::load_state_test(m_json_test_file); }
 };
 
-void register_test_files(const fs::path& root_dir)
+void register_test(const std::string& suite_name, const fs::path& file)
 {
-    std::vector<fs::path> test_files;
-    std::copy_if(fs::recursive_directory_iterator{root_dir}, fs::recursive_directory_iterator{},
-        std::back_inserter(test_files), [](const fs::directory_entry& entry) {
-            return entry.is_regular_file() && entry.path().extension() == ".json";
-        });
-    std::sort(test_files.begin(), test_files.end());
-    for (const auto& p : test_files)
+    testing::RegisterTest(suite_name.c_str(), file.stem().string().c_str(), nullptr, nullptr,
+        file.string().c_str(), 0, [file]() -> testing::Test* { return new StateTest(file); });
+}
+
+void register_test_files(const fs::path& root)
+{
+    if (is_directory(root))
     {
-        const auto d = fs::relative(p, root_dir);
-        testing::RegisterTest(d.parent_path().string().c_str(), d.stem().string().c_str(), nullptr,
-            nullptr, p.string().c_str(), 0, [p]() -> testing::Test* { return new StateTest(p); });
+        std::vector<fs::path> test_files;
+        std::copy_if(fs::recursive_directory_iterator{root}, fs::recursive_directory_iterator{},
+            std::back_inserter(test_files), [](const fs::directory_entry& entry) {
+                return entry.is_regular_file() && entry.path().extension() == ".json";
+            });
+        std::sort(test_files.begin(), test_files.end());
+
+        for (const auto& p : test_files)
+            register_test(fs::relative(p, root).parent_path().string(), p);
+    }
+    else  // Assume regular file.
+    {
+        register_test(root.parent_path().string(), root);
     }
 }
 }  // namespace
 
+
 int main(int argc, char* argv[])
 {
     testing::InitGoogleTest(&argc, argv);  // Process GoogleTest flags.
-
-    if (argc != 2)
-    {
-        std::cerr << "Missing argument with the path to the tests directory\n";
-        return -1;
-    }
-
-    register_test_files(argv[1]);
+    for (int i = 1; i < argc; ++i)
+        register_test_files(argv[i]);
     return RUN_ALL_TESTS();
 }
