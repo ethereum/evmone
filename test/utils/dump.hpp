72276a405d030ed4d99f56df45a9d9b34a163c73
Paweł Bylica <pawel@hepcolgum.band>
2022-11-29 23:55:05 +0100

    test: Remove unused testutils-dump helper library

--- test/utils/dump.hpp
+++
@@ -1,8 +0,0 @@
-// evmone: Fast Ethereum Virtual Machine implementation
-// Copyright 2019 The evmone Authors.
-// SPDX-License-Identifier: Apache-2.0
-#pragma once
-
-#include <evmone/advanced_analysis.hpp>
-
-void dump(const evmone::advanced::AdvancedCodeAnalysis& analysis);
