523dc943c76ef634c7056523dfdf041c44fece14
Andrei Maiboroda <andrei@ethereum.org>
2022-12-07 17:50:55 +0100

    Undefine RJUMP* instructions in non-EOF code
    Co-authored-by: Paweł Bylica <pawel@hepcolgum.band>

--- lib/evmone/baseline_instruction_table.cpp
+++ lib/evmone/baseline_instruction_table.cpp
@@ -7,21 +7,33 @@
 
 namespace evmone::baseline
 {
-const CostTable& get_baseline_cost_table(evmc_revision rev) noexcept
+namespace
 {
-    static constexpr auto cost_tables = []() noexcept {
-        std::array<CostTable, EVMC_MAX_REVISION + 1> tables{};
-        for (size_t r = EVMC_FRONTIER; r <= EVMC_MAX_REVISION; ++r)
+constexpr auto eof_cost_tables = []() noexcept {
+    std::array<CostTable, EVMC_MAX_REVISION + 1> tables{};
+    for (size_t r = EVMC_FRONTIER; r <= EVMC_MAX_REVISION; ++r)
+    {
+        auto& table = tables[r];
+        for (size_t i = 0; i < table.size(); ++i)
         {
-            auto& table = tables[r];
-            for (size_t i = 0; i < table.size(); ++i)
-            {
-                table[i] = instr::gas_costs[r][i];  // Include instr::undefined in the table.
-            }
+            table[i] = instr::gas_costs[r][i];  // Include instr::undefined in the table.
         }
-        return tables;
-    }();
+    }
+    return tables;
+}();
 
-    return cost_tables[rev];
+constexpr auto legacy_cost_tables = []() noexcept {
+    auto tables = eof_cost_tables;
+    tables[EVMC_SHANGHAI][OP_RJUMP] = instr::undefined;
+    tables[EVMC_SHANGHAI][OP_RJUMPI] = instr::undefined;
+    return tables;
+}();
+
+}  // namespace
+
+const CostTable& get_baseline_cost_table(evmc_revision rev, uint8_t eof_version) noexcept
+{
+    const auto& tables = (eof_version == 0) ? legacy_cost_tables : eof_cost_tables;
+    return tables[rev];
 }
 }  // namespace evmone::baseline
