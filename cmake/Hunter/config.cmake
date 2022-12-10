03475fb49adc4a782e4516e4b992093c4355d6f5
Paweł Bylica <pawel@hepcolgum.band>
2022-12-10 15:43:23 +0100

    cmake: Upgrade intx

--- cmake/Hunter/config.cmake
+++ cmake/Hunter/config.cmake
@@ -4,6 +4,14 @@
 
 include(hunter_cmake_args)
 
+hunter_config(
+    intx
+    VERSION LS
+    URL https://github.com/chfast/intx/archive/load_store.tar.gz
+    SHA1 ca6e35bfcdf0d3273c61dff0983e32a74d2887ce
+)
+
+
 hunter_cmake_args(
     ethash
     CMAKE_ARGS -DETHASH_BUILD_ETHASH=OFF
