042d9c17dca1179f3465dc8248b2ca1c04755b9c
Alex Beregszaszi <alex@rtfs.hu>
2023-02-04 16:24:26 +0100

    override

--- cmake/Hunter/config.cmake
+++ cmake/Hunter/config.cmake
@@ -4,9 +4,16 @@
 
 include(hunter_cmake_args)
 
+hunter_config(
+    ethash
+    VERSION 1.0.1
+    URL https://github.com/chfast/ethash/archive/v1.0.0.tar.gz
+    SHA1 75e64b885be0ad90f0fad8e8e718f02d4b0edac8
+)
+
 hunter_cmake_args(
     ethash
-    CMAKE_ARGS ETHASH_BUILD_ETHASH=NO ETHASH_BUILD_TESTS=NO
+    CMAKE_ARGS ETHASH_BUILD_ETHASH=OFF ETHASH_BUILD_TESTS=OFF
 )
 
 hunter_config(
