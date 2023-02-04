0a204d4a30965a4d7934e897872de766ee29365a
Alex Beregszaszi <alex@rtfs.hu>
2023-02-04 16:28:37 +0100

    Ethash fixed

--- cmake/Hunter/config.cmake
+++ cmake/Hunter/config.cmake
@@ -4,16 +4,13 @@
 
 include(hunter_cmake_args)
 
+# Need to use this, because overriding via hunter_cmake_args is not working,
+# due to precendence rules.
 hunter_config(
     ethash
-    VERSION 1.0.1
+    VERSION 1.0.0-evmone
     URL https://github.com/chfast/ethash/archive/v1.0.0.tar.gz
     SHA1 75e64b885be0ad90f0fad8e8e718f02d4b0edac8
-    CMAKE_ARGS ETHASH_BUILD_ETHASH=OFF ETHASH_BUILD_TESTS=OFF
-)
-
-hunter_cmake_args(
-    ethash
     CMAKE_ARGS ETHASH_BUILD_ETHASH=OFF ETHASH_BUILD_TESTS=OFF
 )
 
