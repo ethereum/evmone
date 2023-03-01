21b57d92add5b34bac362b81553314b638807920
rodiazet <radek.zagorowicz@gmail.com>
2023-03-01 10:45:03 +0100

    test: Disable all unit tests for Advanced
    Co-authored-by: Pawel Bylica <pawel@ethereum.org>

--- test/unittests/evm_fixture.cpp
+++ test/unittests/evm_fixture.cpp
@@ -26,7 +26,7 @@ const char* print_vm_name(const testing::TestParamInfo<evmc::VM*>& info) noexcep
 }  // namespace
 
 INSTANTIATE_TEST_SUITE_P(
-    evmone, evm, testing::Values(&advanced_vm, &baseline_vm, &bnocgoto_vm), print_vm_name);
+    evmone, evm, testing::Values(/*&advanced_vm,*/ &baseline_vm, &bnocgoto_vm), print_vm_name);
 
 bool evm::is_advanced() noexcept
 {
