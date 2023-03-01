020837371c26e74ff01663ec2360cb9cf53204fd
Paweł Bylica <pawel@ethereum.org>
2023-03-01 16:09:16 +0100

    move bn254 to lib

--- test/unittests/evmmax_bn254_test.cpp
+++ test/unittests/evmmax_bn254_test.cpp
@@ -3,12 +3,10 @@
 // SPDX-License-Identifier: Apache-2.0
 
 #include "../utils/utils.hpp"
-#include <evmmax/evmmax.hpp>
+#include <evmmax/bn254.hpp>
 #include <gtest/gtest.h>
 
-using namespace intx;
-
-constexpr auto BN254Mod = 0x30644e72e131a029b85045b68181585d97816a916871ca8d3c208c16d87cfd47_u256;
+using namespace evmmax::bn254;
 
 struct TestCase
 {
@@ -112,116 +110,6 @@ static const TestCase
                     "15bf2bb17880144b5d1cd2b1f46eff9d617bffd1ca57c37fb5a49bd84e53cf66049c797f9ce0d17083deb32b5e36f2ea2a212ee036598dd7624c168993d1355f"_hex},
 
 };
-
-namespace
-{
-uint256 expmod(const evmmax::ModArith<uint256>& s, uint256 base, uint256 exponent) noexcept
-{
-    auto result = s.to_mont(1);
-
-    while (exponent != 0)
-    {
-        if ((exponent & 1) != 0)
-            result = s.mul(result, base);
-        base = s.mul(base, base);
-        exponent >>= 1;
-    }
-    return result;
-}
-
-uint256 inv(const evmmax::ModArith<uint256>& s, const uint256& x) noexcept
-{
-    return expmod(s, x, s.mod - 2);
-}
-
-struct Point
-{
-    uint256 x;
-    uint256 y;
-
-    friend bool operator==(const Point& a, const Point& b) noexcept
-    {
-        // TODO(intx): C++20 operator<=> = default does not work for uint256.
-        return a.x == b.x && a.y == b.y;
-    }
-};
-
-bool is_at_infinity(const Point& pt) noexcept
-{
-    return pt.x == 0 && pt.y == 0;
-}
-
-bool validate(const Point& pt) noexcept
-{
-    if (is_at_infinity(pt))
-        return true;
-
-    const evmmax::ModArith s{BN254Mod};
-    const auto xm = s.to_mont(pt.x);
-    const auto ym = s.to_mont(pt.y);
-    const auto y2 = s.mul(ym, ym);
-    const auto x2 = s.mul(xm, xm);
-    const auto x3 = s.mul(x2, xm);
-    const auto _3 = s.to_mont(3);
-    const auto x3_3 = s.add(x3, _3);
-    return y2 == x3_3;
-}
-
-Point bn254_add(const Point& pt1, const Point& pt2) noexcept
-{
-    if (is_at_infinity(pt1))
-        return pt2;
-    if (is_at_infinity(pt2))
-        return pt1;
-
-    const evmmax::ModArith s{BN254Mod};
-
-    // https://eprint.iacr.org/2015/1060 algorithm 2.
-    // Simplified with z3 == 1, a == 0, b3 == 9.
-
-    auto b3 = s.to_mont(9);
-
-    auto x1 = s.to_mont(pt1.x);
-    auto y1 = s.to_mont(pt1.y);
-
-    auto x2 = s.to_mont(pt2.x);
-    auto y2 = s.to_mont(pt2.y);
-
-    uint256 x3, y3, z3, t0, t1, t3, t4, t5;
-
-    t0 = s.mul(x1, x2);
-    t1 = s.mul(y1, y2);
-    t3 = s.add(x2, y2);
-    t4 = s.add(x1, y1);
-    t3 = s.mul(t3, t4);
-    t4 = s.add(t0, t1);
-    t3 = s.sub(t3, t4);
-    t4 = s.add(x2, x1);
-    t5 = s.add(y2, y1);
-    x3 = s.sub(t1, b3);
-    z3 = s.add(t1, b3);
-    y3 = s.mul(x3, z3);
-    t1 = s.add(t0, t0);
-    t1 = s.add(t1, t0);
-    t4 = s.mul(b3, t4);
-    t0 = s.mul(t1, t4);
-    y3 = s.add(y3, t0);
-    t0 = s.mul(t5, t4);
-    x3 = s.mul(t3, x3);
-    x3 = s.sub(x3, t0);
-    t0 = s.mul(t3, t1);
-    z3 = s.mul(t5, z3);
-    z3 = s.add(z3, t0);
-
-    auto z3_inv = inv(s, z3);
-    x3 = s.mul(x3, z3_inv);
-    y3 = s.mul(y3, z3_inv);
-    x3 = s.from_mont(x3);
-    y3 = s.from_mont(y3);
-
-    return {x3, y3};
-}
-}  // namespace
 
 TEST(evmmax, bn254_validate_inputs)
 {
