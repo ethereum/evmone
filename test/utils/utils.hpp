6caacb69b02889213545347871569a8ed286bfef
Paweł Bylica <pawel@ethereum.org>
2023-03-15 09:45:42 +0000

    test: Drop hexx (#582)
    Hexx is a hex encoding with some de-duplication features. Previously
    used in benchmarks. The only remaining usage was unit tests to allow
    spaces in the hex (already provided by from_spaced_hex or ""_hex).
    Therefore, remove hexx because not used any more.

--- test/utils/utils.hpp
+++ test/utils/utils.hpp
@@ -27,15 +27,3 @@ inline bytes operator""_hex(const char* s, size_t size)
 {
     return from_spaced_hex({s, size}).value();
 }
-
-/// Decodes the hexx encoded string.
-///
-/// The hexx encoding format is the hex format (base 16) with the extension
-/// for run-length encoding. The parser replaces expressions like
-///     `(` <num_repetitions> `x` <element> `)`
-/// with `<element>` repeated `<num_repetitions>` times.
-/// E.g. `(2x1d3)` is `1d31d3` in hex.
-///
-/// @param hexx  The hexx encoded string.
-/// @return      The decoded bytes.
-bytes from_hexx(const std::string& hexx);
