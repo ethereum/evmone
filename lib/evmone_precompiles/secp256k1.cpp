f2d571b6a11b29e081048e8203b840250d273d5d
Paweł Bylica <pawel@ethereum.org>
2023-06-09 16:33:48 +0200

    precompiles: Add secp256k1 stub

---
+++ lib/evmone_precompiles/secp256k1.cpp
@@ -0,0 +1,7 @@
+// evmone: Fast Ethereum Virtual Machine implementation
+// Copyright 2023 The evmone Authors.
+// SPDX-License-Identifier: Apache-2.0
+#include "secp256k1.hpp"
+
+namespace evmmax::secp256k1
+{}
---
+++ lib/evmone_precompiles/secp256k1.hpp
@@ -0,0 +1,11 @@
+// evmone: Fast Ethereum Virtual Machine implementation
+// Copyright 2023 The evmone Authors.
+// SPDX-License-Identifier: Apache-2.0
+#pragma once
+
+#include "ecc.hpp"
+
+namespace evmmax::secp256k1
+{
+using namespace intx;
+}  // namespace evmmax::secp256k1
