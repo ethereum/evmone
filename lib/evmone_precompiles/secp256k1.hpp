7f8f583bd53dd2c2ae9b6a56cba8c97b0937f903
Andrei Maiboroda <andrei@ethereum.org>
2023-06-09 19:46:29 +0200

    secp256k1: Implement square root for prime field
    Main part generated by addchain (https://github.com/mmcloughlin/addchain).
    addchain search 0x3fffffffffffffffffffffffffffffffffffffffffffffffffffffffbfffff0c > secp256k1_sqrt.acc
    addchain gen -tmpl expmod.tmpl secp256k1_sqrt.acc > secp256k1_sqrt.cpp

--- lib/evmone_precompiles/secp256k1.hpp
+++ lib/evmone_precompiles/secp256k1.hpp
@@ -4,6 +4,7 @@
 #pragma once
 
 #include "ecc.hpp"
+#include <optional>
 
 namespace evmmax::secp256k1
 {
@@ -18,5 +19,13 @@ inline constexpr auto FieldPrime =
 /// Computes 1/x mod P modular inversion by computing modular exponentiation x^(P-2),
 /// where P is ::FieldPrime.
 uint256 field_inv(const ModArith<uint256>& m, const uint256& x) noexcept;
+
+/// Square root for secp256k1 prime field.
+///
+/// Computes √x mod P by computing modular exponentiation x^((P+1)/4),
+/// where P is ::FieldPrime.
+///
+/// @return Square root of x if it exists, std::nullopt otherwise.
+std::optional<uint256> field_sqrt(const ModArith<uint256>& m, const uint256& x) noexcept;
 
 }  // namespace evmmax::secp256k1
