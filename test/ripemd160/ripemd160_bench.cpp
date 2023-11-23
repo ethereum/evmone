7b65db6c7fa1a9a8641c529a67e6a23d1985b484
Paweł Bylica <pawel@ethereum.org>
2023-11-23 19:39:46 +0100

    add empty benchmark

--- test/ripemd160/ripemd160_bench.cpp
+++ test/ripemd160/ripemd160_bench.cpp
@@ -10,7 +10,15 @@ static void ripemd160_compress(benchmark::State& state)
     for (auto _ : state)
         rmd160_compress(ctx, xxx);
 }
-
 BENCHMARK(ripemd160_compress);
+
+static void ripemd160_empty(benchmark::State& state)
+{
+    uint8_t out[20];
+
+    for (auto _ : state)
+        ripemd160(out, nullptr, 0);
+}
+BENCHMARK(ripemd160_empty);
 
 BENCHMARK_MAIN();
