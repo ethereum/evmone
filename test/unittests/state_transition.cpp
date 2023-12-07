4ae67fe16ebc2a7b1a00789382660b3c702884c1
Paweł Bylica <pawel@ethereum.org>
2023-12-07 09:01:48 +0100

    test: Export state transition tests to JSON
    Add option to export the "state_transition" test suite
    to JSON State Test format.
    
    The current version of the export feature has limitations:
    - tests with invalid transaction are not exported
    - not every test property is properly exported
    - some exported tests fail in `evmone-statetest`

--- test/unittests/state_transition.cpp
+++ test/unittests/state_transition.cpp
@@ -2,8 +2,19 @@
 // Copyright 2023 The evmone Authors.
 // SPDX-License-Identifier: Apache-2.0
 
+#ifdef _MSC_VER
+// Disable warning C4996: 'getenv': This function or variable may be unsafe.
+#define _CRT_SECURE_NO_WARNINGS
+#endif
+
 #include "state_transition.hpp"
 #include <evmone/eof.hpp>
+#include <test/state/mpt_hash.hpp>
+#include <test/statetest/statetest.hpp>
+#include <filesystem>
+#include <fstream>
+
+namespace fs = std::filesystem;
 
 namespace evmone::test
 {
@@ -65,6 +76,7 @@ void state_transition::TearDown()
             EXPECT_TRUE(pre.get_accounts().contains(addr)) << "unexpected account " << addr;
         }
 
+        // TODO: Export also tests with invalid transactions.
         return;  // Do not check anything else.
     }
 
@@ -127,5 +139,67 @@ void state_transition::TearDown()
     {
         EXPECT_TRUE(expect.post.contains(addr)) << "unexpected account " << addr;
     }
+
+    if (const auto export_dir = std::getenv("EVMONE_EXPORT_TESTS"); export_dir != nullptr)
+        export_state_test(receipt, state, export_dir);
+}
+
+namespace
+{
+/// Creates the file path for the exported test based on its name.
+fs::path get_export_test_path(const testing::TestInfo& test_info, std::string_view export_dir)
+{
+    const std::string_view test_suite_name{test_info.test_suite_name()};
+
+    const auto stem = fs::path{test_info.file()}.stem().string();
+    auto filename = std::string_view{stem};
+    if (filename.starts_with(test_suite_name))
+        filename.remove_prefix(test_suite_name.size() + 1);
+    if (filename.ends_with("_test"))
+        filename.remove_suffix(5);
+
+    const auto dir = fs::path{export_dir} / test_suite_name / filename;
+
+    fs::create_directories(dir);
+    return dir / (std::string{test_info.name()} + ".json");
+}
+}  // namespace
+
+void state_transition::export_state_test(
+    const TransactionReceipt& receipt, const State& post, std::string_view export_dir)
+{
+    const auto& test_info = *testing::UnitTest::GetInstance()->current_test_info();
+
+    json::json j;
+    auto& jt = j[test_info.name()];
+
+    auto& jenv = jt["env"];
+    jenv["currentNumber"] = hex0x(block.number);
+    jenv["currentTimestamp"] = hex0x(block.timestamp);
+    jenv["currentGasLimit"] = hex0x(block.gas_limit);
+    jenv["currentCoinbase"] = hex0x(block.coinbase);
+    jenv["currentBaseFee"] = hex0x(block.base_fee);
+
+    jt["pre"] = to_json(pre.get_accounts());
+
+    auto& jtx = jt["transaction"];
+    if (tx.to.has_value())
+        jtx["to"] = hex0x(*tx.to);
+    jtx["sender"] = hex0x(tx.sender);
+    jtx["secretKey"] = hex0x(SenderSecretKey);
+    jtx["nonce"] = hex0x(tx.nonce);
+    jtx["maxFeePerGas"] = hex0x(tx.max_gas_price);
+    jtx["maxPriorityFeePerGas"] = hex0x(tx.max_priority_gas_price);
+
+    jtx["data"][0] = hex0x(tx.data);
+    jtx["gasLimit"][0] = hex0x(tx.gas_limit);
+    jtx["value"][0] = hex0x(tx.value);
+
+    auto& jpost = jt["post"][evmc::to_string(rev)][0];
+    jpost["indexes"] = {{"data", 0}, {"gas", 0}, {"value", 0}};
+    jpost["hash"] = hex0x(mpt_hash(post.get_accounts()));
+    jpost["logs"] = hex0x(logs_hash(receipt.logs));
+
+    std::ofstream{get_export_test_path(test_info, export_dir)} << std::setw(2) << j;
 }
 }  // namespace evmone::test
