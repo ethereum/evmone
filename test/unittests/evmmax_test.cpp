26f96655c979e4653fa9f4d66f488f0107767cd2
rodiazet <radek.zagorowicz@gmail.com>
2024-01-09 11:08:22 +0100

    test: Add evmmax unit tests

--- test/unittests/evmmax_test.cpp
+++ test/unittests/evmmax_test.cpp
@@ -2,12 +2,14 @@
 // Copyright 2023 The evmone Authors.
 // SPDX-License-Identifier: Apache-2.0
 
+#include "evm_fixture.hpp"
 #include <evmmax/evmmax.hpp>
 #include <gtest/gtest.h>
 #include <array>
 
 using namespace intx;
 using namespace evmmax;
+using evmone::test::evm;
 
 // TODO(intx): Add ""_u384.
 inline constexpr auto operator""_u384(const char* s)
@@ -138,5 +140,171 @@ TYPED_TEST(evmmax_test, mul)
             const auto p = m.from_mont(pm);
             EXPECT_EQ(p, expected);
         }
+    }
+}
+
+namespace
+{
+template <typename UintT>
+inline bytecode create_test_bytecode()
+{
+    constexpr auto size = sizeof(UintT);
+    return calldatacopy(push(0), push(0), push(size * 3)) + setupx(3, size, 0, 1) +
+           storex(2, size, 0) + mulmodx(2, 1, 0) + loadx(1, 2, size * 3) + ret(size * 3, size);
+}
+
+}  // namespace
+
+TEST_P(evm, exec_bn254_test)
+{
+    using namespace evmone::test;
+
+    if (evm::is_advanced())
+        return;
+
+    evm::rev = EVMC_PRAGUE;  /// TODO: Use EVMC_EVMMAX
+
+    const ModA<uint256, BN254Mod> m;
+
+    uint8_t calldata[3 * sizeof(uint256)];
+    intx::be::unsafe::store(&calldata[0], BN254Mod);
+
+    const auto values = get_test_values(m);
+
+    const auto code = create_test_bytecode<uint256>();
+
+    for (const auto& x : values)
+    {
+        for (const auto& y : values)
+        {
+            const auto expected = udivrem(umul(x, y), m.mod).rem;
+
+            intx::be::unsafe::store(&calldata[32], x);
+            intx::be::unsafe::store(&calldata[64], y);
+
+            execute(1000, code, {calldata, 96});
+            EXPECT_EQ(result.status_code, EVMC_SUCCESS);
+            EXPECT_OUTPUT_INT(expected);
+        }
+    }
+}
+
+TEST_P(evm, exec_bls_test)
+{
+    using namespace evmone::test;
+
+    if (evm::is_advanced())
+        return;
+
+    evm::rev = EVMC_PRAGUE;  /// TODO: Use EVMC_EVMMAX
+
+    const ModA<uint384, BLS12384Mod> m;
+
+    constexpr auto size = sizeof(uint384);
+    uint8_t calldata[3 * size];
+    intx::be::unsafe::store(&calldata[0], BLS12384Mod);
+
+    const auto values = get_test_values(m);
+
+    const auto code = create_test_bytecode<uint384>();
+
+    for (const auto& x : values)
+    {
+        for (const auto& y : values)
+        {
+            const auto expected = udivrem(umul(x, y), m.mod).rem;
+
+            intx::be::unsafe::store(&calldata[size], x);
+            intx::be::unsafe::store(&calldata[size * 2], y);
+
+            execute(1000, code, {calldata, size * 3});
+            EXPECT_EQ(result.status_code, EVMC_SUCCESS);
+            ASSERT_EQ(result.output_size, size);
+            EXPECT_EQ(intx::be::unsafe::load<uint384>(result.output_data), expected);
+        }
+    }
+}
+
+TEST_P(evm, exec_invalid_test)
+{
+    using namespace evmone::test;
+
+    if (evm::is_advanced())
+        return;
+
+    evm::rev = EVMC_PRAGUE;  /// TODO: Use EVMC_EVMMAX
+
+    {
+        // Even modulus
+        constexpr auto size = sizeof(uint256);
+        uint8_t calldata[3 * size];
+
+        const auto code = create_test_bytecode<uint256>();
+        intx::be::unsafe::store(&calldata[0], BN254Mod + 1);
+        execute(1000, code, {calldata, size * 3});
+        EXPECT_EQ(result.status_code, EVMC_FAILURE);
+    }
+
+    {
+        // Modulus too big
+        constexpr auto size = sizeof(intx::uint<4160>);
+        uint8_t calldata[3 * size];
+
+        const auto code = create_test_bytecode<intx::uint<4160>>();
+        intx::be::unsafe::store(&calldata[0], intx::uint<4160>(7));
+        execute(1000, code, {calldata, size * 3});
+        EXPECT_EQ(result.status_code, EVMC_FAILURE);
+    }
+
+    {
+        // Too many value slots
+        constexpr auto size = sizeof(uint256);
+        uint8_t calldata[size];
+
+        const auto code = calldatacopy(push(0), push(0), push(size)) + setupx(257, size, 0, 1);
+        intx::be::unsafe::store(&calldata[0], BN254Mod);
+        execute(1000, code, {calldata, size});
+        EXPECT_EQ(result.status_code, EVMC_FAILURE);
+    }
+
+    {
+        // not enough gas
+        constexpr auto size = sizeof(uint256);
+        uint8_t calldata[3 * size];
+
+        const auto code = create_test_bytecode<uint256>();
+        intx::be::unsafe::store(&calldata[0], BN254Mod);
+        execute(45, code, {calldata, size * 3});
+        EXPECT_EQ(result.status_code, EVMC_OUT_OF_GAS);
+    }
+
+    {
+        // Too much evmmax memory used
+        constexpr auto size = sizeof(intx::uint<2048>);
+        uint8_t calldata[size * 3];
+
+        const auto code = calldatacopy(push(0), push(0), push(size)) + setupx(1, size, 0, 1) +
+                          setupx(256, size, 0, 2);
+        intx::be::unsafe::store(&calldata[0], intx::uint<2048>(BN254Mod));
+        execute(1000, code, {calldata, size});
+        EXPECT_EQ(result.status_code, EVMC_FAILURE);
+    }
+
+    {
+        // No active modulus
+        execute(1000, addmodx(0, 0, 1));
+        EXPECT_EQ(result.status_code, EVMC_FAILURE);
+
+        execute(1000, mulmodx(0, 0, 2));
+        EXPECT_EQ(result.status_code, EVMC_FAILURE);
+
+        execute(1000, submodx(0, 0, 2));
+        EXPECT_EQ(result.status_code, EVMC_FAILURE);
+
+        execute(1000, loadx(1, 0, 0));
+        EXPECT_EQ(result.status_code, EVMC_FAILURE);
+
+        execute(1000, storex(1, 0, 0));
+        EXPECT_EQ(result.status_code, EVMC_FAILURE);
     }
 }
