1012ec1433cb77494b7de1275f8d78475ea22505
pdobacz <5735525+pdobacz@users.noreply.github.com>
2024-02-12 09:46:35 +0100

    Assume all existing EOF code is valid

--- test/unittests/state_transition_extcode_test.cpp
+++ test/unittests/state_transition_extcode_test.cpp
@@ -9,20 +9,6 @@ using namespace evmc::literals;
 using namespace evmone::test;
 
 constexpr auto target = 0xfffffffffffffffffffffffffffffffffffffffe_address;
-constexpr auto ones = 0x1111111111111111111111111111111111111111111111111111111111111111_bytes32;
-
-TEST_F(state_transition, legacy_extcodesize_eof_cancun)
-{
-    pre.insert(target, {.code = eof_bytecode("FE")});
-
-    rev = EVMC_CANCUN;
-    tx.to = To;
-    pre.insert(*tx.to, {
-                           .code = bytecode(push(target) + sstore(1, OP_EXTCODESIZE)),
-                       });
-    expect.post[*tx.to].storage[0x01_bytes32] = 0x14_bytes32;
-    expect.post[target].exists = true;
-}
 
 TEST_F(state_transition, legacy_extcodesize_eof)
 {
@@ -34,19 +20,6 @@ TEST_F(state_transition, legacy_extcodesize_eof)
                            .code = bytecode(push(target) + sstore(1, OP_EXTCODESIZE)),
                        });
     expect.post[*tx.to].storage[0x01_bytes32] = 0x02_bytes32;
-    expect.post[target].exists = true;
-}
-
-TEST_F(state_transition, legacy_extcodehash_eof_cancun)
-{
-    pre.insert(target, {.code = eof_bytecode("FE")});
-
-    rev = EVMC_CANCUN;
-    tx.to = To;
-    pre.insert(*tx.to, {
-                           .code = bytecode(push(target) + sstore(1, OP_EXTCODEHASH)),
-                       });
-    expect.post[*tx.to].storage[0x01_bytes32] = keccak256(bytecode(eof_bytecode("FE")));
     expect.post[target].exists = true;
 }
 
@@ -63,23 +36,10 @@ TEST_F(state_transition, legacy_extcodehash_eof)
     expect.post[target].exists = true;
 }
 
-TEST_F(state_transition, legacy_extcodecopy_eof_cancun)
-{
-    pre.insert(target, {.code = eof_bytecode("FE")});
-
-    rev = EVMC_CANCUN;
-    tx.to = To;
-    pre.insert(*tx.to, {
-                           .code = bytecode(mstore(0, ones) + push(20) + push0() + push0() +
-                                            push(target) + OP_EXTCODECOPY + sstore(1, mload(0))),
-                       });
-    expect.post[*tx.to].storage[0x01_bytes32] =
-        0xef000101000402000100010400000000800000fe111111111111111111111111_bytes32;
-    expect.post[target].exists = true;
-}
-
 TEST_F(state_transition, legacy_extcodecopy_eof)
 {
+    constexpr auto ones =
+        0x1111111111111111111111111111111111111111111111111111111111111111_bytes32;
     pre.insert(target, {.code = eof_bytecode("FE")});
 
     rev = EVMC_PRAGUE;
