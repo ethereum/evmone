megaeth-labs/evmone-compiler

Implement "CALLS 2.0"

lib/evmone/instructions_traits.hpp
@@ -171,6 +171,9 @@ constexpr inline GasCostTable gas_costs = []() noexcept {
     table[EVMC_CANCUN][OP_RJUMPV] = 4;
     table[EVMC_CANCUN][OP_CALLF] = 5;
     table[EVMC_CANCUN][OP_RETF] = 3;
+    table[EVMC_CANCUN][OP_CALL2] = 700;
+    table[EVMC_CANCUN][OP_DELEGATECALL2] = 700;
+    table[EVMC_CANCUN][OP_STATICCALL2] = 700;
 
     table[EVMC_PRAGUE] = table[EVMC_CANCUN];
 
@@ -381,8 +384,11 @@ constexpr inline std::array<Traits, 256> traits = []() noexcept {
     table[OP_CALLCODE] = {"CALLCODE", 0, false, 7, -6, EVMC_FRONTIER};
     table[OP_RETURN] = {"RETURN", 0, true, 2, -2, EVMC_FRONTIER};
     table[OP_DELEGATECALL] = {"DELEGATECALL", 0, false, 6, -5, EVMC_HOMESTEAD};
+    table[OP_CALL2] = {"CALL2", 0, false, 4, -3, EVMC_CANCUN};
+    table[OP_DELEGATECALL2] = {"DELEGATECALL2", 0, false, 3, -2, EVMC_CANCUN};
     table[OP_CREATE2] = {"CREATE2", 0, false, 4, -3, EVMC_CONSTANTINOPLE};
     table[OP_STATICCALL] = {"STATICCALL", 0, false, 6, -5, EVMC_BYZANTIUM};
+    table[OP_STATICCALL2] = {"STATICCALL2", 0, false, 3, -2, EVMC_CANCUN};
     table[OP_CALLF] = {"CALLF", 2, false, 0, 0, EVMC_CANCUN};
     table[OP_RETF] = {"RETF", 0, true, 0, 0, EVMC_CANCUN};
     table[OP_REVERT] = {"REVERT", 0, true, 2, -2, EVMC_BYZANTIUM};

ethereum/evmone

lib/evmone/instructions_traits.hpp
@@ -184,6 +184,9 @@ constexpr inline GasCostTable gas_costs = []() noexcept {
     table[EVMC_PRAGUE][OP_DATASIZE] = 2;
     table[EVMC_PRAGUE][OP_DATACOPY] = 3;
     table[EVMC_PRAGUE][OP_RETURNDATALOAD] = 3;
+    table[EVMC_PRAGUE][OP_CALL2] = 700;
+    table[EVMC_PRAGUE][OP_DELEGATECALL2] = 700;
+    table[EVMC_PRAGUE][OP_STATICCALL2] = 700;
 
     return table;
 }();
@@ -403,7 +406,10 @@ constexpr inline std::array<Traits, 256> traits = []() noexcept {
     table[OP_DELEGATECALL] = {"DELEGATECALL", 0, false, 6, -5, EVMC_HOMESTEAD};
     table[OP_CREATE2] = {"CREATE2", 0, false, 4, -3, EVMC_CONSTANTINOPLE};
     table[OP_RETURNDATALOAD] = {"RETURNDATALOAD", 0, false, 1, 0, EVMC_PRAGUE};
+    table[OP_CALL2] = {"CALL2", 0, false, 4, -3, EVMC_PRAGUE};
+    table[OP_DELEGATECALL2] = {"DELEGATECALL2", 0, false, 3, -2, EVMC_PRAGUE};
     table[OP_STATICCALL] = {"STATICCALL", 0, false, 6, -5, EVMC_BYZANTIUM};
+    table[OP_STATICCALL2] = {"STATICCALL2", 0, false, 3, -2, EVMC_PRAGUE};
     table[OP_CALLF] = {"CALLF", 2, false, 0, 0, EVMC_PRAGUE};
     table[OP_RETF] = {"RETF", 0, true, 0, 0, EVMC_PRAGUE};
     table[OP_JUMPF] = {"JUMPF", 2, true, 0, 0, EVMC_PRAGUE};