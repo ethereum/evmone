fac56827efd787bf0571e7da5ff5d605db798513
Paweł Bylica <pawel@ethereum.org>
2024-02-29 00:52:02 +0100

    fuzz

--- test/fuzzer/jumpdest_analysis_fuzz.cpp
+++ test/fuzzer/jumpdest_analysis_fuzz.cpp
@@ -3,7 +3,6 @@
 // SPDX-License-Identifier: Apache-2.0
 
 #include "test/experimental/jumpdest_analysis.hpp"
-#include <evmone/analysis.hpp>
 #include <evmone/baseline.hpp>
 
 
@@ -13,7 +12,7 @@ using namespace evmone::experimental;
 namespace
 {
 template <typename T1, typename T2>
-inline void expect_eq(T1 a, T2 b) noexcept
+inline void EXPECT_EQ(T1 a, T2 b) noexcept
 {
     if (a != b)
         __builtin_unreachable();
@@ -21,26 +20,15 @@ inline void expect_eq(T1 a, T2 b) noexcept
 
 constexpr auto tail_code_padding = 100;
 
-inline bool is_jumpdest(const JumpdestMap& a, size_t index) noexcept
-{
-    return (index < a.size() && a[index]);
-}
-
 inline bool is_jumpdest(const bitset32& a, size_t index) noexcept
 {
     return (index < a.size() && a[index]);
-}
-
-[[maybe_unused]] inline bool is_jumpdest(const code_analysis& a, size_t index) noexcept
-{
-    return find_jumpdest(a, static_cast<int>(index)) >= 0;
 }
 }  // namespace
 
 extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t data_size) noexcept
 {
-    const auto a0 = build_jumpdest_map(data, data_size);
-    const auto a1 = analyze(EVMC_FRONTIER, data, data_size);
+    const auto a0 = official_analyze_jumpdests(data, data_size);
     const auto a2 = build_jumpdest_map_vec1(data, data_size);
     const auto v2 = build_jumpdest_map_vec2(data, data_size);
     const auto x3 = build_jumpdest_map_vec3(data, data_size);
@@ -57,29 +45,28 @@ extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t data_size) noe
     const auto s1 = build_jumpdest_map_simd1(data, data_size);
     const auto s2 = build_jumpdest_map_simd2(data, data_size);
     const auto s3 = build_jumpdest_map_simd3(data, data_size);
+    const auto s4 = build_jumpdest_map_simd4(data, data_size);
 
     for (size_t i = 0; i < data_size + tail_code_padding; ++i)
     {
         const bool expected = is_jumpdest(a0, i);
-        expect_eq(is_jumpdest(a1, i), expected);
-        expect_eq(is_jumpdest(a2, i), expected);
-        expect_eq(is_jumpdest(v2, i), expected);
-        expect_eq(is_jumpdest(x3, i), expected);
-        expect_eq(is_jumpdest(v3, i), expected);
-        expect_eq(is_jumpdest(v4, i), expected);
-        expect_eq(is_jumpdest(v5, i), expected);
-        expect_eq(is_jumpdest(v6, i), expected);
-        expect_eq(is_jumpdest(a3, i), expected);
-        expect_eq(is_jumpdest(a4.get(), data_size, i), expected);
-        expect_eq(is_jumpdest(a5.get(), data_size, i), expected);
-        expect_eq(is_jumpdest(a6.get(), data_size, i), expected);
-        expect_eq(is_jumpdest(ic4.get(), data_size, i), expected);
-        expect_eq(is_jumpdest(ic8.get(), data_size, i), expected);
-        expect_eq(is_jumpdest(s1, i), expected);
-        expect_eq(is_jumpdest(s2, i), expected);
-        expect_eq(is_jumpdest(s3, i), expected);
+        EXPECT_EQ(is_jumpdest(a2, i), expected);
+        EXPECT_EQ(is_jumpdest(v2, i), expected);
+        EXPECT_EQ(is_jumpdest(x3, i), expected);
+        EXPECT_EQ(is_jumpdest(v3, i), expected);
+        EXPECT_EQ(is_jumpdest(v4, i), expected);
+        EXPECT_EQ(is_jumpdest(v5, i), expected);
+        EXPECT_EQ(is_jumpdest(v6, i), expected);
+        EXPECT_EQ(is_jumpdest(a3, i), expected);
+        EXPECT_EQ(is_jumpdest(a4.get(), data_size, i), expected);
+        EXPECT_EQ(is_jumpdest(a5.get(), data_size, i), expected);
+        EXPECT_EQ(is_jumpdest(a6.get(), data_size, i), expected);
+        EXPECT_EQ(is_jumpdest(ic4.get(), data_size, i), expected);
+        EXPECT_EQ(is_jumpdest(ic8.get(), data_size, i), expected);
+        EXPECT_EQ(is_jumpdest(s1, i), expected);
+        EXPECT_EQ(is_jumpdest(s2, i), expected);
+        EXPECT_EQ(is_jumpdest(s3, i), expected);
+        EXPECT_EQ(is_jumpdest(s4, i), expected);
     }
-
-
     return 0;
 }
