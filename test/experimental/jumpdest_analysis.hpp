f78852b8a4dfd0170f73b31f483a4e53092548f6
Paweł Bylica <pawel@ethereum.org>
2024-02-28 15:16:23 +0100

    fix build

--- test/experimental/jumpdest_analysis.hpp
+++ test/experimental/jumpdest_analysis.hpp
@@ -9,6 +9,8 @@
 
 namespace evmone::experimental
 {
+using JumpdestMap = std::vector<bool>;
+
 inline bool is_jumpdest(const std::vector<bool>& jumpdest_map, size_t index) noexcept
 {
     return (index < jumpdest_map.size() && jumpdest_map[index]);
@@ -34,7 +36,7 @@ public:
 
     std::size_t size() const noexcept { return size_; }
 
-     bool operator[](std::size_t index) const noexcept
+    bool operator[](std::size_t index) const noexcept
     {
         const auto w = index / bpw;
         const auto x = index % bpw;
@@ -61,7 +63,7 @@ std::vector<bool> build_jumpdest_map_str_avx2_mask2(const uint8_t* code, size_t 
 bitset32 build_jumpdest_map_simd1(const uint8_t* code, size_t code_size);
 bitset32 build_jumpdest_map_simd2(const uint8_t* code, size_t code_size);
 bitset32 build_jumpdest_map_simd3(const uint8_t* code, size_t code_size);
-bitset build_jumpdest_map_bitset1(const uint8_t* code, size_t code_size);
+JumpdestMap build_jumpdest_map_bitset1(const uint8_t* code, size_t code_size);
 std::unique_ptr<uint8_t[]> build_internal_code_v1(const uint8_t* code, size_t code_size);
 std::unique_ptr<uint8_t[]> build_internal_code_v2(const uint8_t* code, size_t code_size);
 std::unique_ptr<uint8_t[]> build_internal_code_v3(const uint8_t* code, size_t code_size);
