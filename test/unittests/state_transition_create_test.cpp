75e4ac5de0819f7b6f5f82d19b2c44037a67c3c0
Paweł Bylica <pawel@ethereum.org>
2024-03-13 14:10:02 +0100

    test: New State transition tests (#806)
    New tests for:
    - contract creation,
    - account touches,
    - empty coinbase,
    - selfdestruct,
    - extcode.

--- test/unittests/state_transition_create_test.cpp
+++ test/unittests/state_transition_create_test.cpp
@@ -42,6 +42,16 @@ TEST_F(state_transition, create_tx)
     expect.post[create_address].code = bytes{0xFE};
 }
 
+TEST_F(state_transition, create_tx_failure)
+{
+    static constexpr auto create_address = 0x3442a1dec1e72f337007125aa67221498cdd759d_address;
+
+    tx.data = bytecode{} + OP_INVALID;
+
+    expect.status = EVMC_INVALID_INSTRUCTION;
+    expect.post[create_address].exists = false;
+}
+
 TEST_F(state_transition, create2_max_nonce)
 {
     tx.to = To;
@@ -154,4 +164,165 @@ TEST_F(state_transition, code_deployment_out_of_gas_refund_f)
     expect.post[created].storage[0x00_bytes32] = 0x00_bytes32;
     expect.post[To].storage[0x00_bytes32] = to_bytes32(created);
     expect.gas_used = 83140;
+}
+
+TEST_F(state_transition, create_tx_collision)
+{
+    static constexpr auto CREATED = 0x3442a1dec1e72f337007125aa67221498cdd759d_address;
+
+    pre.insert(CREATED, {.nonce = 2});
+
+    expect.status = EVMC_FAILURE;
+    expect.post[CREATED].nonce = 2;
+}
+
+TEST_F(state_transition, create_collision)
+{
+    static constexpr auto CREATED = 0x8bbc3514477d75ec797bbe4e19d7961660bb849c_address;
+
+    tx.to = To;
+    pre.insert(*tx.to, {.code = create()});
+    pre.insert(CREATED, {.nonce = 2});
+
+    expect.post[*tx.to].nonce = pre.get(*tx.to).nonce + 1;
+    expect.post[CREATED].nonce = pre.get(CREATED).nonce;
+}
+
+TEST_F(state_transition, create_collision_revert)
+{
+    static constexpr auto CREATED = 0x8bbc3514477d75ec797bbe4e19d7961660bb849c_address;
+
+    tx.to = To;
+    pre.insert(*tx.to, {.code = create() + OP_INVALID});
+    pre.insert(CREATED, {.nonce = 2});
+
+    expect.status = EVMC_INVALID_INSTRUCTION;
+    expect.post[*tx.to].nonce = pre.get(*tx.to).nonce;
+    expect.post[CREATED].nonce = pre.get(CREATED).nonce;
+}
+
+TEST_F(state_transition, create_prefunded_revert)
+{
+    static constexpr auto CREATED = 0x8bbc3514477d75ec797bbe4e19d7961660bb849c_address;
+
+    tx.to = To;
+    pre.insert(*tx.to, {.code = create() + OP_INVALID});
+    pre.insert(CREATED, {.balance = 2});
+
+    expect.status = EVMC_INVALID_INSTRUCTION;
+    expect.post[*tx.to].nonce = pre.get(*tx.to).nonce;
+    expect.post[CREATED].nonce = pre.get(CREATED).nonce;
+}
+
+TEST_F(state_transition, create_revert)
+{
+    static constexpr auto CREATED = 0x8bbc3514477d75ec797bbe4e19d7961660bb849c_address;
+
+    tx.to = To;
+    pre.insert(*tx.to, {.code = create() + OP_INVALID});
+
+    expect.status = EVMC_INVALID_INSTRUCTION;
+    expect.post[*tx.to].nonce = pre.get(*tx.to).nonce;
+    expect.post[CREATED].exists = false;
+}
+
+TEST_F(state_transition, create_revert_sd)
+{
+    rev = EVMC_SPURIOUS_DRAGON;
+    block.base_fee = 0;
+    static constexpr auto CREATED = 0x8bbc3514477d75ec797bbe4e19d7961660bb849c_address;
+
+    tx.to = To;
+    pre.insert(*tx.to, {.code = create() + OP_INVALID});
+
+    expect.status = EVMC_INVALID_INSTRUCTION;
+    expect.post[*tx.to].nonce = pre.get(*tx.to).nonce;
+    expect.post[CREATED].exists = false;
+}
+
+TEST_F(state_transition, create_revert_tw)
+{
+    rev = EVMC_TANGERINE_WHISTLE;
+    block.base_fee = 0;
+    static constexpr auto CREATED = 0x8bbc3514477d75ec797bbe4e19d7961660bb849c_address;
+
+    tx.to = To;
+    pre.insert(*tx.to, {.code = create() + OP_INVALID});
+
+    expect.status = EVMC_INVALID_INSTRUCTION;
+    expect.post[*tx.to].nonce = pre.get(*tx.to).nonce;
+    expect.post[CREATED].exists = false;
+}
+
+TEST_F(state_transition, create_collision_empty_revert)
+{
+    static constexpr auto CREATED = 0x8bbc3514477d75ec797bbe4e19d7961660bb849c_address;
+
+    tx.to = To;
+    pre.insert(*tx.to, {.code = create() + OP_INVALID});
+    pre.insert(CREATED, {});
+
+    expect.status = EVMC_INVALID_INSTRUCTION;
+    expect.post[*tx.to].nonce = pre.get(*tx.to).nonce;
+    expect.post[CREATED].exists = true;
+}
+
+TEST_F(state_transition, create_collision_empty_revert_tw)
+{
+    rev = EVMC_TANGERINE_WHISTLE;
+    block.base_fee = 0;
+    static constexpr auto CREATED = 0x8bbc3514477d75ec797bbe4e19d7961660bb849c_address;
+
+    tx.to = To;
+    pre.insert(*tx.to, {.code = create() + OP_INVALID});
+    pre.insert(CREATED, {});
+
+    expect.status = EVMC_INVALID_INSTRUCTION;
+    expect.post[*tx.to].nonce = pre.get(*tx.to).nonce;
+    expect.post[CREATED].exists = true;
+}
+
+TEST_F(state_transition, touch_create_collision_empty_revert)
+{
+    static constexpr auto CREATED = 0x11f72042f0f1c9d8a1aeffc3680d0b41dd7769a7_address;
+    static constexpr auto REVERT_PROXY = 0x94_address;
+
+    tx.to = To;
+    pre.insert(*tx.to, {.code = call(CREATED) + call(REVERT_PROXY).gas(0xffff)});
+    pre.insert(REVERT_PROXY, {.code = create() + OP_INVALID});
+
+    expect.post[*tx.to].nonce = pre.get(*tx.to).nonce;
+    expect.post[CREATED].exists = false;
+    expect.post[REVERT_PROXY].exists = true;
+}
+
+TEST_F(state_transition, touch_create_collision_empty_revert_tw)
+{
+    rev = EVMC_TANGERINE_WHISTLE;
+    block.base_fee = 0;
+    static constexpr auto CREATED = 0x11f72042f0f1c9d8a1aeffc3680d0b41dd7769a7_address;
+    static constexpr auto REVERT_PROXY = 0x94_address;
+
+    tx.to = To;
+    pre.insert(*tx.to, {.code = call(CREATED) + call(REVERT_PROXY).gas(0xffff)});
+    pre.insert(REVERT_PROXY, {.code = create() + OP_INVALID});
+
+    expect.post[*tx.to].nonce = pre.get(*tx.to).nonce;
+    expect.post[CREATED].exists = true;
+    expect.post[REVERT_PROXY].exists = true;
+}
+
+TEST_F(state_transition, created_code_hash)
+{
+    const auto runtime_code = bytes{0xc0};
+    ASSERT_EQ(runtime_code.size(), 1);
+    const auto initcode = mstore8(0, push(runtime_code)) + ret(0, runtime_code.size());
+    tx.to = To;
+    pre.insert(To,
+        {.code = mstore(0, push(initcode)) + create().input(32 - initcode.size(), initcode.size()) +
+                 sstore(0, bytecode{OP_EXTCODEHASH})});
+
+    const auto created = compute_create_address(To, pre.get(To).nonce);
+    expect.post[created].code = runtime_code;
+    expect.post[To].storage[0x00_bytes32] = keccak256(runtime_code);
 }
