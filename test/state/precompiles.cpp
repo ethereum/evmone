544783346a2aa8a7e0a16b3571db255b52ed6b63
rodiazet <radek.zagorowicz@gmail.com>
2024-04-02 13:08:04 +0200

    evmmax: Benchmark ate pairing
    Co-authored-by: Paweł Bylica <pawel@ethereum.org>

--- test/state/precompiles.cpp
+++ test/state/precompiles.cpp
@@ -348,6 +348,55 @@ ExecutionResult ecmul_execute(const uint8_t* input, size_t input_size, uint8_t* 
         return {EVMC_PRECOMPILE_FAILURE, 0};
 }
 
+ExecutionResult ecpairing_execute(const uint8_t* input, size_t input_size, uint8_t* output,
+    [[maybe_unused]] size_t output_size) noexcept
+{
+    assert(output_size >= 32);
+
+    static constexpr size_t PAIR_SIZE = 192;
+
+    if (input_size % PAIR_SIZE != 0)
+        return {EVMC_PRECOMPILE_FAILURE, 0};
+
+    if (const auto n_pairs = input_size / PAIR_SIZE; n_pairs > 0)
+    {
+        std::vector<std::pair<evmmax::bn254::Point, evmmax::bn254::ExtPoint>> pairs;
+        pairs.reserve(n_pairs);
+        auto input_idx = input;
+        for (size_t i = 0; i < n_pairs; ++i)
+        {
+            const evmmax::bn254::Point p{
+                intx::be::unsafe::load<intx::uint256>(input_idx),
+                intx::be::unsafe::load<intx::uint256>(input_idx + 32),
+            };
+
+            const evmmax::bn254::ExtPoint q{
+                {intx::be::unsafe::load<intx::uint256>(input_idx + 96),
+                    intx::be::unsafe::load<intx::uint256>(input_idx + 64)},
+                {intx::be::unsafe::load<intx::uint256>(input_idx + 160),
+                    intx::be::unsafe::load<intx::uint256>(input_idx + 128)},
+            };
+            pairs.emplace_back(p, q);
+            input_idx += PAIR_SIZE;
+        }
+
+        const auto res = evmmax::bn254::pairing_check(pairs);
+
+        if (res.has_value())
+        {
+            intx::be::unsafe::store(output, res.value() ? intx::uint256{1} : intx::uint256{0});
+            return {EVMC_SUCCESS, 64};
+        }
+        else
+            return {EVMC_PRECOMPILE_FAILURE, 0};
+    }
+    else
+    {
+        intx::be::unsafe::store(output, intx::uint256{1});
+        return {EVMC_SUCCESS, 32};
+    }
+}
+
 ExecutionResult identity_execute(const uint8_t* input, size_t input_size, uint8_t* output,
     [[maybe_unused]] size_t output_size) noexcept
 {
