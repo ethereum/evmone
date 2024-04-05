d46e3b9fff5367b98c70b3a490bfdb37566f08d6
Paweł Bylica <pawel@ethereum.org>
2024-04-05 14:18:33 +0200

    precompiles: Add the BLAKE2bf precompile
    Implement [EIP-152](https://eips.ethereum.org/EIPS/eip-152)
    "BLAKE2 compression function `F` precompile" by exposing
    the `evmone::crypto::blake2b_compress()`.

--- test/state/precompiles_internal.hpp
+++ test/state/precompiles_internal.hpp
@@ -40,4 +40,6 @@ ExecutionResult ecadd_execute(
     const uint8_t* input, size_t input_size, uint8_t* output, size_t output_size) noexcept;
 ExecutionResult ecmul_execute(
     const uint8_t* input, size_t input_size, uint8_t* output, size_t output_size) noexcept;
+ExecutionResult blake2bf_execute(
+    const uint8_t* input, size_t input_size, uint8_t* output, size_t output_size) noexcept;
 }  // namespace evmone::state
