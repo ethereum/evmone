f1da0811c555ed8277af4cc51a1cb492789af35d
Paweł Bylica <pawel@ethereum.org>
2024-06-08 18:19:52 +0200

    precompiles: Copy SHA256 implementation from Silkworm
    Copy the C implementation of SHA256 from Silkworm to evmone
    without any modifications.
    The original source file:
    https://github.com/erigontech/silkworm/blob/capi-0.19.0/silkworm/core/crypto/sha256.c

---
+++ lib/evmone_precompiles/sha256.h
@@ -0,0 +1,31 @@
+/*
+Copyright 2022 The Silkworm Authors
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+#pragma once
+
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+
+#if defined(__cplusplus)
+extern "C" {
+#endif
+
+void silkworm_sha256(uint8_t hash[32], const uint8_t* input, size_t len, bool use_cpu_extensions);
+
+#if defined(__cplusplus)
+}
+#endif
