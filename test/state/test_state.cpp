8b5df1486ae3e7a524fc7f3c5c181935a106ed8b
Paweł Bylica <pawel@ethereum.org>
2024-10-10 14:27:41 +0200

    test: Implement `BlockHashes` interface for tests
    Add `TestBlockHashes` implementing the `state::BlockHashes` interface.

--- test/state/test_state.cpp
+++ test/state/test_state.cpp
@@ -59,6 +59,17 @@ bytes32 TestState::get_storage(const address& addr, const bytes32& key) const no
     return (it != storage.end()) ? it->second : bytes32{};
 }
 
+bytes32 TestBlockHashes::get_block_hash(int64_t block_number) const noexcept
+{
+    if (const auto& it = known_block_hashes_.find(block_number); it != known_block_hashes_.end())
+        return it->second;
+
+    // Convention for testing: if the block hash in unknown return the predefined "fake" value.
+    // https://github.com/ethereum/go-ethereum/blob/v1.12.2/tests/state_test_util.go#L432
+    const auto s = std::to_string(block_number);
+    return keccak256({reinterpret_cast<const uint8_t*>(s.data()), s.size()});
+}
+
 [[nodiscard]] std::variant<state::TransactionReceipt, std::error_code> transition(TestState& state,
     const state::BlockInfo& block, const state::Transaction& tx, evmc_revision rev, evmc::VM& vm,
     int64_t block_gas_left, int64_t blob_gas_left)
