8547c71bcf2f3ba7a6adafb4087e073df3e8bec9
Andrei Maiboroda <andrei@ethereum.org>
2024-11-06 11:58:07 +0700

    Support EIP-7702 set code transactions

--- test/state/state.cpp
+++ test/state/state.cpp
@@ -10,10 +10,19 @@
 #include <evmone/eof.hpp>
 #include <algorithm>
 
+using namespace intx;
+
 namespace evmone::state
 {
 namespace
 {
+constexpr auto SECP256K1N = 0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEBAAEDCE6AF48A03BBFD25E8CD0364141_u256;
+constexpr auto SECP256K1N_OVER_2 = SECP256K1N / 2;
+// EIP-7702: The cost of authorization that sets delegation to an account that didn't exist before
+constexpr auto AUTHORIZATION_EMPTY_ACCOUNT_COST = 25000;
+// EIP-7702: The cost of authorization that sets delegation to an account that already exists
+constexpr auto AUTHORIZATION_BASE_COST = 12500;
+
 inline constexpr int64_t num_words(size_t size_in_bytes) noexcept
 {
     return static_cast<int64_t>((size_in_bytes + 31) / 32);
@@ -46,11 +55,13 @@ int64_t compute_tx_intrinsic_cost(evmc_revision rev, const Transaction& tx) noex
     static constexpr auto create_tx_cost = 53000;
     static constexpr auto initcode_word_cost = 2;
     const auto is_create = !tx.to.has_value();  // Covers also EOF creation txs.
+    const auto auth_list_cost =
+        static_cast<int64_t>(tx.authorization_list.size()) * AUTHORIZATION_EMPTY_ACCOUNT_COST;
     const auto initcode_cost =
         is_create && rev >= EVMC_SHANGHAI ? initcode_word_cost * num_words(tx.data.size()) : 0;
     const auto tx_cost = is_create && rev >= EVMC_HOMESTEAD ? create_tx_cost : call_tx_cost;
     return tx_cost + compute_tx_data_cost(rev, tx.data) + compute_access_list_cost(tx.access_list) +
-           initcode_cost;
+           auth_list_cost + initcode_cost;
 }
 
 evmc_message build_message(
@@ -76,6 +87,11 @@ evmc_message build_message(
         .code_address = recipient,
         .code = nullptr,
         .code_size = 0};
+}
+
+constexpr bool is_code_delegated(bytes_view code) noexcept
+{
+    return code.starts_with(DELEGATION_MAGIC);
 }
 }  // namespace
 
@@ -320,12 +336,22 @@ std::variant<int64_t, std::error_code> validate_transaction(const StateView& sta
             return make_error_code(BLOB_GAS_LIMIT_EXCEEDED);
         break;
 
+    case Transaction::Type::set_code:
+        if (rev < EVMC_PRAGUE)
+            return make_error_code(TX_TYPE_NOT_SUPPORTED);
+        if (!tx.to.has_value())
+            return make_error_code(CREATE_SET_CODE_TX);
+        if (tx.authorization_list.empty())
+            return make_error_code(EMPTY_AUTHORIZATION_LIST);
+        break;
+
     default:;
     }
 
     switch (tx.type)
     {
     case Transaction::Type::blob:
+    case Transaction::Type::set_code:
     case Transaction::Type::eip1559:
         if (rev < EVMC_LONDON)
             return make_error_code(TX_TYPE_NOT_SUPPORTED);
@@ -355,6 +381,7 @@ std::variant<int64_t, std::error_code> validate_transaction(const StateView& sta
     const auto sender_acc = state_view.get_account(tx.sender).value_or(
         StateView::Account{.code_hash = Account::EMPTY_CODE_HASH});
 
+    // TODO this is relaxed for 7702
     if (sender_acc.code_hash != Account::EMPTY_CODE_HASH)
         return make_error_code(SENDER_NOT_EOA);  // Origin must not be a contract (EIP-3607).
 
@@ -430,6 +457,62 @@ TransactionReceipt transition(const StateView& state_view, const BlockInfo& bloc
     assert(sender_acc.nonce < Account::NonceMax);  // Required for valid tx.
     ++sender_acc.nonce;                            // Bump sender nonce.
 
+    auto delegation_refund = 0;
+
+    for (const auto& auth : tx.authorization_list)
+    {
+        if (auth.chain_id != 0 && auth.chain_id != tx.chain_id)
+            continue;
+
+        if (auth.nonce == Account::NonceMax)
+            continue;
+
+        // Check if signer could be ecrecovered.
+        if (!auth.signer.has_value())
+            continue;
+
+        if (auth.s > SECP256K1N_OVER_2)
+            continue;
+
+        // Check if authority exists.
+        auto* authority_ptr = state.find(*auth.signer);
+        if (authority_ptr != nullptr && !authority_ptr->is_empty())
+        {
+            authority_ptr->access_status = EVMC_ACCESS_WARM;
+
+            // Skip if authority has non-delegated code.
+            if (authority_ptr->code_hash != Account::EMPTY_CODE_HASH &&
+                !is_code_delegated(state.get_code(*auth.signer)))
+                continue;
+
+            // Skip if authorization nonce is incorrect.
+            if (auth.nonce != authority_ptr->nonce)
+                continue;
+
+            // Refund if authority account creation is not needed.
+            static constexpr int64_t EXISTING_AUTHORITY_REFUND =
+                AUTHORIZATION_EMPTY_ACCOUNT_COST - AUTHORIZATION_BASE_COST;
+            delegation_refund += EXISTING_AUTHORITY_REFUND;
+        }
+        else
+        {
+            // Create authority account.
+            // It is still considered empty at this point until nonce increment, but already warm.
+            authority_ptr = &state.get_or_insert(
+                *auth.signer, {.erase_if_empty = true, .access_status = EVMC_ACCESS_WARM});
+
+            // Skip if authorization nonce is incorrect.
+            if (auth.nonce != 0)
+                continue;
+        }
+
+        authority_ptr->code.reserve(std::size(DELEGATION_MAGIC) + std::size(auth.addr.bytes));
+        authority_ptr->code = DELEGATION_MAGIC;
+        authority_ptr->code += auth.addr;
+
+        ++authority_ptr->nonce;
+    }
+
     const auto base_fee = (rev >= EVMC_LONDON) ? block.base_fee : 0;
     assert(tx.max_gas_price >= base_fee);                   // Required for valid tx.
     assert(tx.max_gas_price >= tx.max_priority_gas_price);  // Required for valid tx.
@@ -472,7 +555,7 @@ TransactionReceipt transition(const StateView& state_view, const BlockInfo& bloc
 
     const auto max_refund_quotient = rev >= EVMC_LONDON ? 5 : 2;
     const auto refund_limit = gas_used / max_refund_quotient;
-    const auto refund = std::min(result.gas_refund, refund_limit);
+    const auto refund = std::min(delegation_refund + result.gas_refund, refund_limit);
     gas_used -= refund;
     assert(gas_used > 0);
 
--- test/state/transaction.hpp
+++ test/state/transaction.hpp
@@ -14,6 +14,20 @@ namespace evmone::state
 {
 using AccessList = std::vector<std::pair<address, std::vector<bytes32>>>;
 
+struct Authorization
+{
+    uint64_t chain_id = 0;
+    address addr;
+    uint64_t nonce = 0;
+    /// Signer is empty if r, s, v cannot be ecrecovered.
+    std::optional<address> signer;
+    intx::uint256 r;
+    intx::uint256 s;
+    intx::uint256 v;
+};
+
+using AuthorizationList = std::vector<Authorization>;
+
 struct Transaction
 {
     /// The type of the transaction.
@@ -36,6 +50,10 @@ struct Transaction
         /// The typed blob transaction (with array of blob hashes).
         /// Introduced by EIP-4844 https://eips.ethereum.org/EIPS/eip-4844.
         blob = 3,
+
+        /// The typed set code transaction (with authorization list).
+        /// Introduced by EIP-7702 https://eips.ethereum.org/EIPS/eip-7702.
+        set_code = 4,
     };
 
     /// Returns amount of blob gas used by this transaction
@@ -61,6 +79,7 @@ struct Transaction
     intx::uint256 r;
     intx::uint256 s;
     uint8_t v = 0;
+    AuthorizationList authorization_list;
 };
 
 struct Log
