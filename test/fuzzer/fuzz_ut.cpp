0e0d37fea83d1dde8a12475646903930b3f2e74f
Paweł Bylica <pawel@ethereum.org>
2025-01-21 16:54:54 +0100

    repro asan failure

--- test/fuzzer/fuzz_ut.cpp
+++ test/fuzzer/fuzz_ut.cpp
@@ -1,5 +1,5 @@
-#include <evmc/evmc.hpp>
-#include <glaze/glaze.hpp>
+// #include "fuzz_types.hpp"
+#include <glaze/json.hpp>
 #include <boost/ut.hpp>
 
 struct Test
@@ -11,16 +11,33 @@ int main()
 {
     using namespace boost::ut;
 
-    "escape_control_characters"_test = [] {
-        Test t;
-        t.s["\001"] = 0;
-        std::string buf;
-        const auto write_ec = glz::write_json(t, buf);
-        expect(!write_ec);
-        expect(buf != "{\"s\":{\"\001\":0}}") << buf;
+    // "escape_control_characters"_test = [] {
+    //     Test t;
+    //     t.s["\001"] = 0;
+    //     std::string buf;
+    //     const auto write_ec = glz::write_json(t, buf);
+    //     expect(!write_ec);
+    //     expect(buf != "{\"s\":{\"\001\":0}}") << buf;
+    //
+    //     const auto read_ec = glz::read_json(t, buf);
+    //     expect(!read_ec) << glz::format_error(read_ec, buf);
+    // };
 
-        const auto read_ec = glz::read_json(t, buf);
-        expect(!read_ec) << glz::format_error(read_ec, buf);
+    "out_of_buf_read"_test = [] {
+        const std::string_view data = R"({"x":"")";
+        const auto heap_buf = std::make_unique_for_overwrite<char[]>(data.size());
+        std::ranges::copy(data, heap_buf.get());
+        const std::string_view buf{heap_buf.get(), data.size()};
+
+        static constexpr glz::opts OPTS{
+            .null_terminated = false,
+            .error_on_unknown_keys = false,
+            .minified = true,
+        };
+
+        struct {} t;
+        const auto ec = glz::read<OPTS>(t, buf);
+        expect(ec);
     };
 
     return 0;
