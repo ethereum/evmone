4e9cfc7eb9af568227d75d684bad2a54d5053efb
Paweł Bylica <pawel@ethereum.org>
2025-02-17 08:56:16 +0100

    Upgrade intx to 0.12.1 and ethash to 1.1.0

--- cmake/Hunter/config.cmake
+++ cmake/Hunter/config.cmake
@@ -10,8 +10,15 @@ hunter_cmake_args(
 )
 
 hunter_config(
+    ethash
+    VERSION 1.1.0
+    URL https://github.com/chfast/ethash/archive/v1.1.0.tar.gz
+    SHA1 b5625c876d7de3997800a9d7546f0657a7fdb3af
+)
+
+hunter_config(
     intx
-    VERSION 0.12.0
-    URL https://github.com/chfast/intx/archive/v0.12.0.tar.gz
-    SHA1 18a64e8e88c50d53325d906c9211daef905b97f4
+    VERSION 0.12.1
+    URL https://github.com/chfast/intx/archive/v0.12.1.tar.gz
+    SHA1 b2465f217b0289c36579668537df4ce2f84547f0
 )
