cacfc8622291d70c3e7d5f20d6c6d71ebb8d9ba5
rodiazet <radek.zagorowicz@gmail.com>
2025-04-22 11:05:46 +0200

    precompile: Add extended explanation for pairing check precompile API. Add unit tests.

--- test/unittests/evmmax_bn254_pairing_test.cpp
+++ test/unittests/evmmax_bn254_pairing_test.cpp
@@ -167,3 +167,86 @@ TEST(evmmax, bn254_pairing_invalid_input)
         EXPECT_EQ(pairing_check(input), std::nullopt);
     }
 }
+
+// Case taken from https://www.evm.codes/precompiled
+TEST(evmmax_bn254, evm_codes_example)
+{
+    // Pair 1
+    const auto p1 = Point{
+        0x2cf44499d5d27bb186308b7af7af02ac5bc9eeb6a3d147c186b21fb1b76e18da_u256,
+        0x2c0f001f52110ccfe69108924926e45f0b0c868df0e7bde1fe16d3242dc715f6_u256,
+    };
+    const auto q1 = ExtPoint{
+        {
+            0x22606845ff186793914e03e21df544c34ffe2f2f3504de8a79d9159eca2d98d9_u256,
+            0x1fb19bb476f6b9e44e2a32234da8212f61cd63919354bc06aef31e3cfaff3ebc_u256,
+        },
+        {
+            0x2fe02e47887507adf0ff1743cbac6ba291e66f59be6bd763950bb16041a0a85e_u256,
+            0x2bd368e28381e8eccb5fa81fc26cf3f048eea9abfdd85d7ed3ab3698d63e4f90_u256,
+        },
+    };
+
+    // Pair 2
+    const auto p2 = Point{
+        0x0000000000000000000000000000000000000000000000000000000000000001_u256,
+        0x30644e72e131a029b85045b68181585d97816a916871ca8d3c208c16d87cfd45_u256,
+    };
+    const auto q2 = ExtPoint{
+        {
+            0x091058a3141822985733cbdddfed0fd8d6c104e9e9eff40bf5abfef9ab163bc7_u256,
+            0x1971ff0471b09fa93caaf13cbf443c1aede09cc4328f5a62aad45f40ec133eb4_u256,
+        },
+        {
+            0x23a8eb0b0996252cb548a4487da97b02422ebc0e834613f954de6c7e0afdc1fc_u256,
+            0x2a23af9a5ce2ba2796c1f4e453a370eb0af8c212d9dc9acd8fc02c2e907baea2_u256,
+        },
+    };
+
+    const std::vector<std::pair<Point, ExtPoint>> pairs = {{p1, q1}, {p2, q2}};
+    const auto result = pairing_check(pairs);
+
+    ASSERT_TRUE(result.has_value()) << "Pairing check should return a value.";
+    EXPECT_EQ(result.value(), true) << "Pairing result should be true, matching Solidity.";
+}
+
+TEST(evmmax_bn254, evm_codes_example_changed_order)
+{
+    // Pair 1
+    const auto p1 = Point{
+        0x2cf44499d5d27bb186308b7af7af02ac5bc9eeb6a3d147c186b21fb1b76e18da_u256,
+        0x2c0f001f52110ccfe69108924926e45f0b0c868df0e7bde1fe16d3242dc715f6_u256,
+    };
+    const auto q1 = ExtPoint{
+        {
+            0x1fb19bb476f6b9e44e2a32234da8212f61cd63919354bc06aef31e3cfaff3ebc_u256,
+            0x22606845ff186793914e03e21df544c34ffe2f2f3504de8a79d9159eca2d98d9_u256,
+        },
+        {
+            0x2bd368e28381e8eccb5fa81fc26cf3f048eea9abfdd85d7ed3ab3698d63e4f90_u256,
+            0x2fe02e47887507adf0ff1743cbac6ba291e66f59be6bd763950bb16041a0a85e_u256,
+        },
+    };
+
+    // Pair 2
+    const auto p2 = Point{
+        0x0000000000000000000000000000000000000000000000000000000000000001_u256,
+        0x30644e72e131a029b85045b68181585d97816a916871ca8d3c208c16d87cfd45_u256,
+    };
+    const auto q2 = ExtPoint{
+        {
+            0x1971ff0471b09fa93caaf13cbf443c1aede09cc4328f5a62aad45f40ec133eb4_u256,
+            0x091058a3141822985733cbdddfed0fd8d6c104e9e9eff40bf5abfef9ab163bc7_u256,
+        },
+        {
+            0x2a23af9a5ce2ba2796c1f4e453a370eb0af8c212d9dc9acd8fc02c2e907baea2_u256,
+            0x23a8eb0b0996252cb548a4487da97b02422ebc0e834613f954de6c7e0afdc1fc_u256,
+        },
+    };
+
+    const std::vector<std::pair<Point, ExtPoint>> pairs = {{p1, q1}, {p2, q2}};
+    const auto result = pairing_check(pairs);
+
+    ASSERT_TRUE(!result.has_value())
+        << "Pairing check should not return value. Modified points are not on twisted curve.";
+}
