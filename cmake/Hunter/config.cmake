f47cfb872c99899cf6d7da6e66728f3d1339edc9
Paweł Bylica <pawel@ethereum.org>
2025-05-16 12:51:10 +0200

    fix config

--- cmake/Hunter/config.cmake
+++ cmake/Hunter/config.cmake
@@ -16,21 +16,21 @@ hunter_config(
     SHA1 b5625c876d7de3997800a9d7546f0657a7fdb3af
 )
 
+hunter_config(
+    intx
+    VERSION 0.13.0
+    URL https://github.com/chfast/intx/archive/v0.13.0.tar.gz
+    SHA1 3e868e3018fe9f2b2f067442c879e4f312d2d707
+)
+
 # Propagate BENCHMARK_ENABLE_LIBPFM to google/benchmark.
 # https://github.com/google/benchmark/blob/v1.9.3/CMakeLists.txt#L42
 option(BENCHMARK_ENABLE_LIBPFM "Enable performance counters provided by libpfm" OFF)
 
 hunter_config(
-    intx
-    VERSION 0.13.0
-    CMAKE_ARGS BENCHMARK_ENABLE_LIBPFM=${BENCHMARK_ENABLE_LIBPFM}
-    URL https://github.com/chfast/intx/archive/v0.13.0.tar.gz
-    SHA1 3e868e3018fe9f2b2f067442c879e4f312d2d707
-)
-
-hunter_config(
     benchmark
     VERSION 1.9.3
+    CMAKE_ARGS BENCHMARK_ENABLE_LIBPFM=${BENCHMARK_ENABLE_LIBPFM}
     URL https://github.com/google/benchmark/archive/v1.9.3.tar.gz
     SHA1 65aeb420ce456fefca0429d1886e83ed3c88e4e7
 )
