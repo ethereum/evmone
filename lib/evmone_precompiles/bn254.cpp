ac8a6d6083a91e520e9438f320aab3714be1f748
Paweł Bylica <pawel@ethereum.org>
2025-05-30 23:23:08 +0200

    instrument add tests

--- lib/evmone_precompiles/bn254.cpp
+++ lib/evmone_precompiles/bn254.cpp
@@ -3,6 +3,7 @@
 // SPDX-License-Identifier: Apache-2.0
 
 #include "bn254.hpp"
+#include <iostream>
 
 namespace evmmax::bn254
 {
@@ -29,7 +30,42 @@ bool validate(const Point& pt) noexcept
 
 Point add(const Point& p, const Point& q) noexcept
 {
-    return ecc::add(Fp, p, q);
+    const auto expected = ecc::add(Fp, p, q);
+
+    // FIXME: Skip inf for now.
+    if (p.is_inf() || q.is_inf())
+        return expected;
+
+    // FIXME: Skip double for now.
+    if (p == q)
+        return expected;
+
+    using namespace ecc;
+    JacPoint<uint256> jp{Fp.to_mont(p.x), Fp.to_mont(p.y), Fp.to_mont(1)};
+    JacPoint<uint256> jq{Fp.to_mont(q.x), Fp.to_mont(q.y), Fp.to_mont(1)};
+    const auto result = ecc::add(Fp, jp, jq);
+    const auto actual = ecc::to_affine(Fp, result);
+
+    if (actual != expected)
+    {
+        std::cerr << hex(p.x) << "\n" << hex(p.y) << "\n" << hex(q.x) << "\n" << hex(q.y) << "\n\n";
+        std::cerr << hex(expected.x) << "\n"
+                  << hex(result.x) << "\n\n"
+                  << hex(expected.y) << "\n"
+                  << hex(result.y) << "\n\n\n";
+        // assert(false);
+        // std::cerr << "expected: " << expected << std::endl;
+        // std::cerr << "actual: " << actual << std::endl;
+        // std::cerr << "jp: " << jp << std::endl;
+        // std::cerr << "jq: " << jq << std::endl;
+        // std::cerr << "result: " << result << std::endl;
+        // std::cerr << "expected: " << expected << std::endl;
+        // std::cerr << "actual: " << actual << std::endl;
+        // std::cerr << "expected: " << expected << std::endl;
+        // std::cerr << "actual: " << actual << std::endl;
+    }
+
+    return expected;
 }
 
 Point mul(const Point& pt, const uint256& c) noexcept
