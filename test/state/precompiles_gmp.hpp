61a9f94f16699bedd896f2f8872eb9f7b1060aca
Paweł Bylica <pawel@ethereum.org>
2025-07-02 13:55:47 +0200

    precompiles: Use std::span in modexp implementation (#1255)
    Use std::span instead of evmc::bytes_view.

--- test/state/precompiles_gmp.hpp
+++ test/state/precompiles_gmp.hpp
@@ -3,14 +3,14 @@
 // SPDX-License-Identifier: Apache-2.0
 #pragma once
 
-#include <evmc/evmc.hpp>
+#include <cstdint>
+#include <span>
 
 namespace evmone::state
 {
-using evmc::bytes_view;
-
 /// Executes the expmod precompile using the GMP library.
 ///
 /// Requires mod not to be zero (having at least one non-zero byte).
-void expmod_gmp(bytes_view base, bytes_view exp, bytes_view mod, uint8_t* output) noexcept;
+void expmod_gmp(std::span<const uint8_t> base, std::span<const uint8_t> exp,
+    std::span<const uint8_t> mod, uint8_t* output) noexcept;
 }  // namespace evmone::state
--- test/state/precompiles_stubs.hpp
+++ test/state/precompiles_stubs.hpp
@@ -3,13 +3,12 @@
 // SPDX-License-Identifier: Apache-2.0
 #pragma once
 
-#include "precompiles_internal.hpp"
+#include <cstdint>
+#include <span>
 
 namespace evmone::state
 {
-using evmc::bytes;
-using evmc::bytes_view;
-
 /// Executes the expmod precompile for trivial and pre-defined inputs.
-void expmod_stub(bytes_view base, bytes_view exp, bytes_view mod, uint8_t* output) noexcept;
+void expmod_stub(std::span<const uint8_t> base, std::span<const uint8_t> exp,
+    std::span<const uint8_t> mod, uint8_t* output) noexcept;
 }  // namespace evmone::state
